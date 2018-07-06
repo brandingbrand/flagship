be0c24d79208b7e85354585ffff7f9b760920013
Sarah <sarahdenaci@gmail.com>
2018-07-06 14:03:20 -0400

    docs(fscomponents): fix shelf story
    updated CMS values so that default banner would be visible
    addded grid of product items
    
    brandingbrand-source-id: bb3985eac81fa298a7d139e5b5c69ecbda6a4682

--- packages/fscomponents/src/components/__stories__/Shelf.story.tsx
+++ packages/fscomponents/src/components/__stories__/Shelf.story.tsx
@@ -1,13 +1,66 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react'; // tslint:disable-line:no-implicit-dependencies
+import { action } from '@storybook/addon-actions'; // tslint:disable-line:no-implicit-dependencies
+import {
+  object
+  // tslint:disable-next-line no-submodule-imports no-implicit-dependencies
+} from '@storybook/addon-knobs/react';
+import { Grid } from '../Grid';
+import { CommerceTypes } from '@brandingbrand/fscommerce';
+import { ProductItemVerticalList } from '../ProductItem/ProductItemVerticalList';
 import { Shelf } from '../Shelf';
+import Decimal from 'decimal.js';
 
 import { CoreContentManagementSystemProvider } from '@brandingbrand/fsengage';
+
 
 const provider = new CoreContentManagementSystemProvider({
   propertyId: '443',
   environment: 1
 });
+
+const defaultStyle = {
+  padding: 15,
+  font: 8,
+  textAlign: 'center'
+};
+
+const kActionOnPress = 'ProductItemVerticalList onPress';
+
+const testProduct: CommerceTypes.Product = {
+  id: '101',
+  brand: 'Brand',
+  title: 'Product',
+  price: {
+    value: new Decimal('9.99'),
+    currencyCode: 'USD'
+  },
+  originalPrice: {
+    value: new Decimal('14.99'),
+    currencyCode: 'USD'
+  },
+  images: [{ uri: require('./assets/images/greyBox.png') }],
+  review: {
+    id: '101',
+    statistics: {
+      id: '101',
+      averageRating: 4.5,
+      reviewCount: 20
+    },
+    reviews: []
+  },
+  promotions: ['This is a sample promotion!']
+};
+
+const renderProduct = (): JSX.Element => {
+  return (
+    <ProductItemVerticalList
+      {...object('Porduct', testProduct)}
+      style={object('style', defaultStyle)}
+      onPress={action(kActionOnPress)}
+    />
+  );
+};
 
 storiesOf('Shelf', module)
   .add('basic usage', () => (
@@ -15,6 +68,12 @@ storiesOf('Shelf', module)
       provider={provider}
       group='Shop'
       identifier='Banner-Carousel'
-      carouselHeight={150}
-    />
+      carouselHeight={100}
+    >
+      <Grid
+        data={[...Array(4).keys()]}
+        columns={2}
+        renderItem={renderProduct}
+      />
+    </Shelf>
   ));
