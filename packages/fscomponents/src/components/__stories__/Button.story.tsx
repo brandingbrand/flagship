fc0b0656302574a9f5586af5acc1fc55350d9676
Sarah <sarahdenaci@gmail.com>
2018-07-06 11:46:07 -0400

    docs(fscomponents): reorganize action bar and buttons stories
    combine single button and action bar w/ 2 button stories. added a two button w/o action bar option
    so user can see what the action bar effectively does (resizes and separates children)
    
    brandingbrand-source-id: 2f87cfb6ca55fda08ce59b95fe647b72c16c5dd2

--- packages/fscomponents/src/components/__stories__/Button.story.tsx
+++ packages/fscomponents/src/components/__stories__/Button.story.tsx
@@ -1,22 +1,53 @@
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { storiesOf } from '@storybook/react'; // tslint:disable-line:no-implicit-dependencies
 import { action } from '@storybook/addon-actions'; // tslint:disable-line:no-implicit-dependencies
 import {
+  number,
   object,
   text
 // tslint:disable-next-line no-submodule-imports no-implicit-dependencies
 } from '@storybook/addon-knobs/react';
+import { ActionBar } from '../ActionBar';
 import { Button } from '../Button';
 
 const defaultStyle = {
-  width: 250
+  width: 250,
+  margin: 5
 };
 
-storiesOf('Button', module)
-  .add('basic usage', () => (
+const styles = StyleSheet.create({
+  twoButtonViewStyle: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
+});
+
+const renderButton = (): JSX.Element => {
+  return (
     <Button
       onPress={action('onPress')}
       title={text('Title', 'Button Title')}
       style={object('Style', defaultStyle)}
     />
+  );
+};
+
+storiesOf('Button', module)
+  .add('single button', renderButton)
+  .add('two buttons', () => {
+    return (
+      <View style={styles.twoButtonViewStyle}>
+        {renderButton()}
+        {renderButton()}
+      </View>
+    );
+  })
+  .add('two buttons in action bar', () => (
+    <ActionBar
+      separatorWidth={number('Separator Width', 10)}
+    >
+      {renderButton()}
+      {renderButton()}
+    </ActionBar>
   ));
