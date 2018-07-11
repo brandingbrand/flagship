9595281c1683a15035617d24970ab82a850671f8
Sarah <sarahdenaci@gmail.com>
2018-07-11 12:45:31 -0400

    feat(fscomponents): add textbox field status
    Tcomb form native demands that templates be pure functions so the templates import stateful components into them
    
    uses a modified version of the tcomb native stylesheet w/ extra classes for our purposes so as to ensure that users are able to modify the styles by fieldsStyleConfig
    
    fieldPosition is a property on the form component which gets passed to the function that generates the stateful component’s stylesheet

--- packages/fscomponents/src/components/Form/Templates/formBoilerplate.tsx
+++ packages/fscomponents/src/components/Form/Templates/formBoilerplate.tsx
@@ -4,33 +4,73 @@ import { Text } from 'react-native';
 import * as t from 'tcomb-form-native';
 import { Dictionary } from '@brandingbrand/fsfoundation';
 
-export function defaultFormStyle(locals: Dictionary): Dictionary {
+export type ComputeFieldType = (prevField: any) => () => void;
+
+export interface StatefulTextboxProps {
+  locals: Dictionary;
+  // for use w/ custom field templates
+  componentFactory?: any;
+}
+
+export interface StatefulTextboxState {
+  active: boolean;
+  validated: boolean;
+}
+
+export function getColor(state: Dictionary, locals: Dictionary): String {
+  const colors = locals.stylesheet.colors;
+
+  if (state.active) {
+    return colors.active;
+  } else {
+    return locals.hasError ? colors.error : colors.inactive;
+  }
+}
+
+export const successIcon = require('../../../../assets/images/checkmarkValidation.png');
+export const errorIcon = require('../../../../assets/images/alert.png');
+
+export function defaultTextboxStyle(locals: Dictionary): Dictionary {
 
   const stylesheet = locals.stylesheet;
 
-  let formGroupStyle = stylesheet.formGroup.normal;
   let controlLabelStyle = stylesheet.controlLabel.normal;
-  let textboxStyle = stylesheet.textbox.normal;
-  let textboxViewStyle = stylesheet.textboxView.normal;
+  let formGroupStyle = stylesheet.formGroup.normal;
   let helpBlockStyle = stylesheet.helpBlock.normal;
+  let inlineFormGroupStyle = stylesheet.inlineFormGroup.normal;
+  let textboxFullBorderStyle = stylesheet.textboxFullBorder.normal;
+  let textboxInlineStyle = stylesheet.textboxInline.normal;
+  let textboxUnderlineStyle = stylesheet.textboxUnderline.normal;
+  let textboxViewStyle = stylesheet.textboxView.normal;
+
+  const alertStyle = stylesheet.alert;
+  const checkStyle = stylesheet.check;
   const errorBlockStyle = stylesheet.errorBlock;
+  const floatingLabelViewStyle = stylesheet.floatingLabelView;
+  const inlineLabelViewStyle = stylesheet.inlineLabelView;
+  const rightTextboxIconStyle = stylesheet.rightTextboxIcon;
 
   if (locals.hasError) {
-    formGroupStyle = stylesheet.formGroup.error;
     controlLabelStyle = stylesheet.controlLabel.error;
-    textboxStyle = stylesheet.textbox.error;
-    textboxViewStyle = stylesheet.textboxView.error;
+    formGroupStyle = stylesheet.formGroup.error;
     helpBlockStyle = stylesheet.helpBlock.error;
+    inlineFormGroupStyle = stylesheet.inlineFormGroup.error;
+    textboxFullBorderStyle = stylesheet.textboxFullBorder.error;
+    textboxInlineStyle = stylesheet.textboxInline.error;
+    textboxUnderlineStyle = stylesheet.textboxUnderline.error;
+    textboxViewStyle = stylesheet.textboxView.error;
   }
 
   if (locals.editable === false) {
-    textboxStyle = stylesheet.textbox.notEditable;
+    textboxFullBorderStyle = stylesheet.textboxFullBorder.notEditable;
+    textboxInlineStyle = stylesheet.textboxInline.notEditable;
+    textboxUnderlineStyle = stylesheet.textboxUnderline.notEditable;
     textboxViewStyle = stylesheet.textboxView.notEditable;
   }
 
-  let label;
   let error;
   let help;
+  let label;
 
   if (typeof locals.label === 'string') {
     label = <Text style={controlLabelStyle}>{locals.label}</Text>;
@@ -45,26 +85,37 @@ export function defaultFormStyle(locals: Dictionary): Dictionary {
     help = locals.help;
   }
 
-  if (typeof locals.error === 'string') {
-    error = (
-      <Text accessibilityLiveRegion='polite' style={errorBlockStyle}>
-        {locals.error}
-      </Text>
+  const activeErrorField = (
+    <Text accessibilityLiveRegion='polite' style={errorBlockStyle}>
+      &nbsp;
+    </Text>
     );
-  } else if (React.isValidElement(locals.error)) {
-    error = locals.error;
-  }
+
+  error = locals.error ? (
+            <Text accessibilityLiveRegion='polite' style={errorBlockStyle}>
+              {locals.error}
+            </Text>
+            ) : activeErrorField;
 
   return {
-    formGroupStyle,
+    alertStyle,
+    checkStyle,
     controlLabelStyle,
-    textboxStyle,
-    textboxViewStyle,
-    helpBlockStyle,
+    activeErrorField,
+    error,
     errorBlockStyle,
-    label,
+    floatingLabelViewStyle,
+    formGroupStyle,
     help,
-    error
+    helpBlockStyle,
+    inlineFormGroupStyle,
+    inlineLabelViewStyle,
+    label,
+    rightTextboxIconStyle,
+    textboxFullBorderStyle,
+    textboxInlineStyle,
+    textboxUnderlineStyle,
+    textboxViewStyle
   };
-
 }
+
