cc2efb957c213b251e813bd95cceeb6a790208c9
Nathan Sankbeil <nathan.sankbeil@brandingbrand.com>
2018-07-26 08:56:46 -0400

    refactor(fscommerce): remove ReviewsProvider
    The reviews provider added little functionality at the expense of
    increased complexity, especially surrounding types
    
    BREAKING CHANGE: Removed withReviewData HOC, removed second parameter from
    withProductDetailData/withProductIndexData
    
    brandingbrand-source-id: 04b48502e35f9817252a9a1a592541bce2763247

--- packages/fscommerce/src/Review/ReviewDataSource.ts
+++ packages/fscommerce/src/Review/ReviewDataSource.ts
@@ -6,12 +6,59 @@ import {
   ReviewSummary
 } from './ReviewTypes';
 
+import { Product } from '../Commerce/CommerceTypes';
+type ProductReviewIdMapper<T = Product> = (product: T) => string;
+type ProductReviewIdKey<T = Product> = keyof T;
+
 // Interface allows for optional methods
-interface ReviewDataSource {
+export interface ReviewDataSource {
   fetchQuestions?(query: ReviewQuery): Promise<ReviewQuestion[]>;
   fetchReviewDetails(query: ReviewQuery): Promise<ReviewDetails[]>;
   fetchReviewSummary(query: ReviewQuery): Promise<ReviewSummary[]>;
   fetchReviewStatistics(query: ReviewQuery): Promise<ReviewStatistics[]>;
+  mergeReviewDetails(...detailsArray: ReviewDetails[][]): ReviewDetails[];
+  productIdMapper<T = Product>(
+    products: T[],
+    idMap: ProductReviewIdKey<T> | ProductReviewIdMapper<T>
+  ): string[];
 }
 
-export default ReviewDataSource;
+export abstract class AbstractReviewDataSource implements ReviewDataSource {
+  abstract fetchReviewDetails(query: ReviewQuery): Promise<ReviewDetails[]>;
+  abstract fetchReviewSummary(query: ReviewQuery): Promise<ReviewSummary[]>;
+  abstract fetchReviewStatistics(query: ReviewQuery): Promise<ReviewStatistics[]>;
+
+  mergeReviewDetails(...detailsArray: ReviewDetails[][]): ReviewDetails[] {
+    return detailsArray.reduce((merged, details) => {
+      return details.map(detail => {
+        const detailInMerged = merged.find(mergedDetail => mergedDetail.id === detail.id);
+        if (detailInMerged) {
+          detail.reviews = [...detailInMerged.reviews, ...detail.reviews];
+        }
+
+        return detail;
+      });
+    }, []);
+  }
+
+  productIdMapper<T = Product>(
+    products: T[],
+    idMap: ProductReviewIdKey<T> | ProductReviewIdMapper<T>
+  ): string[] {
+    if (typeof idMap === 'string' || typeof idMap === 'number' || typeof idMap === 'symbol') {
+      return products.reduce((ids, product) => {
+        const newIds = [...ids];
+        const keyValue = product[idMap];
+        if (typeof keyValue === 'string') {
+          newIds.push(keyValue);
+        }
+
+        return newIds;
+      }, [] as string[]);
+    } else if (typeof idMap === 'function') {
+      return products.map(idMap);
+    }
+
+    throw new Error('Unable to map products to review ids.');
+  }
+}
