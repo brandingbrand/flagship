e3297bf6ac134f91bd8f14caeff63cd6d432a422
Daniel Clayton <daniel.clayton@brandingbrand.com>
2018-08-29 12:30:43 -0500

    fix(flagship): fix possible memory leak and refactor Android code
    brandingbrand-source-id: 2985fba7193f62c406df3262b3cff6cbf3939d1e

--- packages/flagship/android/app/src/main/java/com/brandingbrand/reactnative/and/flagship/MainActivity.java
+++ packages/flagship/android/app/src/main/java/com/brandingbrand/reactnative/and/flagship/MainActivity.java
@@ -1,21 +1,8 @@
 package com.brandingbrand.reactnative.and.flagship;
 
-import android.app.Activity;
-import android.os.Bundle;
-
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
-    private static Activity mCurrentActivity = null;
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mCurrentActivity = this;
-    }
-
-    public static Activity getActivity(){
-        return mCurrentActivity;
-    }
     @Override
     public int getSplashLayout() {
         return R.layout.splash;
