c402a1be91bf3a07763b3ef249ea5a4756ef89ca
jcploucha <jon.ploucha@brandingbrand.com>
2018-08-30 15:25:09 -0400

    feat(flagship): improve add-keys-ios output
    Added logging before an import occurs to clearly see what
    certficate/identity is being imported as well as the output from the
    `exec` calls to see the status.
    
    Creating a keychain if one exists with the same name will actually crash
    the script, so wrapped that in a `try-catch` block. This shouldn't be a
    problem in CI environments, but allowing to use an already existing
    keychain will be useful to those running the script locally.
    
    Also wrapped the `copyFileSync` command in a `try-catch` block so as to
    output a message similar to the security commands.

--- packages/flagship/src/add-keys-ios.ts
+++ packages/flagship/src/add-keys-ios.ts
@@ -23,46 +23,60 @@ const importedProfilePath =
 const keychainName = process.env.KEYCHAIN || 'login.keychain';
 const keychainPwd = process.env.KEYCHAIN_PWD;
 const keychain = `~/Library/Keychains/${keychainName}`;
-console.log(`keychain is ${keychain}`);
-exec(`security create-keychain -p '${keychainPwd}' ${keychainName}`);
-exec(`security default-keychain -s ${keychainName}`);
-exec(`security unlock-keychain -p '${keychainPwd}' ${keychainName}`);
-exec(`security set-keychain-settings -t 3600 -u ${keychainName}`);
+console.log(`creating ${keychain}`);
+try {
+  exec(`security create-keychain -p '${keychainPwd}' ${keychainName}`);
+} catch (e) {
+  console.log(`error creating ${keychain}`);
+}
+exec(`security default-keychain -s ${keychainName}`, { stdio: [0, 1, 2] });
+exec(`security unlock-keychain -p '${keychainPwd}' ${keychainName}`, { stdio: [0, 1, 2] });
+exec(`security set-keychain-settings -t 3600 -u ${keychainName}`, { stdio: [0, 1, 2] });
 
-// add certificates and provisioning profiles to computer
+// add certificates and provisioning profiles to build environment
+// import wwdr
+console.log(`\nimporting ${buildConfig.wwdrCert}`);
 exec(
   `security import ${certificatePath}/${buildConfig.wwdrCert}.cer -k ${keychain} \
--T /usr/bin/codesign || true`
+-T /usr/bin/codesign || true`, { stdio: [0, 1, 2] }
 );
 
-// add keys for store build
 // - import project .cer
+console.log(`\nimporting ${buildConfig.distributionCert} certificate`);
 exec(
   `security import ${certificatePath}/${buildConfig.distributionCert}.cer -k ${keychain} \
--T /usr/bin/codesign -A || true`
+-T /usr/bin/codesign -A || true`, { stdio: [0, 1, 2] }
 );
+
 // - import project .p12
+console.log(`\nimporting ${buildConfig.distributionCert} .p12`);
 exec(
   `security import ${certificatePath}/${buildConfig.distributionCert}.p12 -k ${keychain} \
--P '${buildConfig.distributionPwd}' -T /usr/bin/codesign -A || true`
+-P '${buildConfig.distributionPwd}' -T /usr/bin/codesign -A || true`, { stdio: [0, 1, 2] }
 );
+
 // - import project .mobileprovision
-const uuid = exec(
-  `grep UUID -A1 -a ${profilePath}/${buildConfig.distributionCert}.mobileprovision | \
-  grep -io "[-A-Z0-9]\\{36\\}"`
-);
-fs.copyFileSync(
-  `${profilePath}/${buildConfig.distributionCert}.mobileprovision`,
-  `${importedProfilePath}/${uuid.toString().trim()}.mobileprovision`
-);
+console.log(`\nimporting ${buildConfig.distributionCert} provisioning profile`);
+try {
+  const uuid = exec(
+    `grep UUID -A1 -a ${profilePath}/${buildConfig.distributionCert}.mobileprovision | \
+    grep -io "[-A-Z0-9]\\{36\\}"`
+  );
+  fs.copyFileSync(
+    `${profilePath}/${buildConfig.distributionCert}.mobileprovision`,
+    `${importedProfilePath}/${uuid.toString().trim()}.mobileprovision`
+  );
+  console.log('1 provisioning profile imported.\n');
+} catch (e) {
+  console.log(`error importing ${buildConfig.distributionCert} provisioning profile\n`);
+}
 
 exec(
-  `security set-key-partition-list -S apple-tool:,apple: -s -k '${keychainPwd}' ${keychain}`
+  `security set-key-partition-list -S apple-tool:,apple: -s -k '${keychainPwd}' ${keychain}`,
+  { stdio: [0, 1, 2] }
 )
 
-console.log(
-  `\nDONE: iOS certificates and provisioning profiles added for [${buildConfig.distributionCert}]\n`
-);
+console.log(`\nDONE: iOS certificates and provisioning profiles added.\n`);
 
 process.exit();
 
