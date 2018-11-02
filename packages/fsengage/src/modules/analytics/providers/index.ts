db0cbdff4e377de441b4edda82f6ad7d76bf2cc1
Nathan Sankbeil <nathan.sankbeil@brandingbrand.com>
2018-11-02 17:28:36 -0400

    feat(fsengage): implement an adapter for Adobe Analytics
    brandingbrand-source-id: 9e17ffbebbc224e4fdafb4bf5a61e0a93cb12bc8

--- packages/fsengage/src/modules/analytics/providers/index.ts
+++ packages/fsengage/src/modules/analytics/providers/index.ts
@@ -3,3 +3,4 @@ export { default as AnalyticsProvider } from './AnalyticsProvider';
 export { default as AdobeMarketingCloudProvider } from './adobe/AdobeMarketingCloudProvider';
 export { default as GoogleAnalyticsProvider } from './google/GoogleAnalyticsProvider';
 export { default as LeanplumProvider } from './leanplum/LeanplumProvider';
+export * from './adobe/AdobeAnalyticsProvider';
