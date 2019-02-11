dda8a87f1e66e54430d93715d805b3d60e9b9b61
David Rhodes <david.rhodes@brandingbrand.com>
2019-02-11 14:41:07 -0500

    fix(fscommerce): fixes sessiontoken typing

--- packages/fscommerce/src/Commerce/sessions/CommerceTokenSessionManager.ts
+++ packages/fscommerce/src/Commerce/sessions/CommerceTokenSessionManager.ts
@@ -36,7 +36,7 @@ export default class CommerceTokenSessionManager extends CommerceSessionManager 
     }
   }
 
-  async getTokenFromLocalStorage(): Promise<SessionToken> {
+  async getTokenFromLocalStorage(): Promise<SessionToken | null | undefined> {
     return SInfo.getItem(CommerceSessionManager.COMMERCE_TOKEN, {})
       .then((tokenString: string) => {
         if (!tokenString) {
