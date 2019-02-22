10f80b9da03da0b633a7090a0e03b8e92c55ec58
David Rhodes <david.rhodes@brandingbrand.com>
2019-02-22 13:33:12 -0500

    refactor(fscomponents): some accordian state to defaultprops

--- packages/pirateship/src/screens/Development.tsx
+++ packages/pirateship/src/screens/Development.tsx
@@ -2,12 +2,15 @@
 // We don't need to worry about translating the element strings
 // in this file since it should only be used in development
 import React, { Component } from 'react';
-
+import { Image, StyleSheet, Text, View } from 'react-native';
 import PSScreenWrapper from '../components/PSScreenWrapper';
 import Row from '../components/PSRow';
 import { NavigatorStyle, ScreenProps } from '../lib/commonTypes';
-import { navBarDefault } from '../styles/Navigation';
+import { navBarTabLanding } from '../styles/Navigation';
+import { fontSize } from '../styles/variables';
 import withAccount, { AccountProps } from '../providers/accountProvider';
+import { Accordion } from '@brandingbrand/fscomponents';
+
 type Screen = import ('react-native-navigation').Screen;
 
 const screens: Screen[] = [
@@ -21,8 +24,17 @@ export interface DevelopmentScreenState {
 
 export interface DevelopmentScreenProps extends ScreenProps, AccountProps {}
 
+const styles = StyleSheet.create({
+  header: {
+    fontSize: fontSize.huge
+  },
+  section: {
+    padding: 15
+  }
+});
+
 class Development extends Component<DevelopmentScreenProps, DevelopmentScreenState> {
-  static navigatorStyle: NavigatorStyle = navBarDefault;
+  static navigatorStyle: NavigatorStyle = navBarTabLanding;
 
   state: DevelopmentScreenState = {
     deviceToken: 'NOT INITIALIZED'
@@ -30,22 +42,69 @@ class Development extends Component<DevelopmentScreenProps, DevelopmentScreenSta
 
   render(): JSX.Element {
     const { navigator } = this.props;
+    const title = <Text>Menu Item</Text>;
+    const icons = {
+      closed: require('../../assets/images/alert.png'),
+      open: require('../../assets/images/check.png')
+    };
+    const content = (
+      <View>
+        <Text>Sub Menu Item</Text>
+      </View>
+    );
+    const imageContent = (
+      <View>
+        <Text>Sub Menu Item</Text>
+        <Image
+          resizeMode='contain'
+          source={{ uri: 'https://via.placeholder.com/350x150' }}
+          style={{ height: 150, width: 350 }}
+        />
+      </View>
+    );
+
     return (
       <PSScreenWrapper
         hideGlobalBanner={true}
         navigator={navigator}
       >
-        {screens.map((screen, i) => (
+        <View style={styles.section}>
+          <Text style={styles.header}>{'PSRow Component'}</Text>
+          {screens.map((screen, i) => (
+            <Row
+              key={i}
+              title={screen.title || `Screen ${i}`}
+              onPress={this.goTo(screen)}
+            />
+          ))}
           <Row
-            key={i}
-            title={screen.title || `Screen ${i}`}
-            onPress={this.goTo(screen)}
+            title='Sign Out without clearing saved credentials'
+            onPress={this.softSignOut}
           />
-        ))}
-        <Row
-          title='Sign Out without clearing saved credentials'
-          onPress={this.softSignOut}
-        />
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.header}>{'Accordion Component'}</Text>
+          <Accordion
+            title={title}
+            content={content}
+          />
+          <Accordion
+            title={title}
+            content={imageContent}
+          />
+          <Accordion
+            title={title}
+            content={content}
+            iconFormat={'arrow'}
+          />
+          <Accordion
+            title={title}
+            content={content}
+            iconFormat={'image'}
+            openIconImage={icons.open}
+            closedIconImage={icons.closed}
+          />
+        </View>
       </PSScreenWrapper>
     );
   }
