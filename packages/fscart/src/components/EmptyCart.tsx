1b8f528e56bd389c5b7a9109bcc3a6a00a8e192f
David Rhodes <david@davidrhodes.work>
2019-03-17 17:13:45 -0400

    refactor(fscart): empty and summary to sfc
    brandingbrand-source-id: d42d454ca0f978c847c7bcb384dd4e8c88f60fd8

--- packages/fscart/src/components/EmptyCart.tsx
+++ packages/fscart/src/components/EmptyCart.tsx
@@ -1,9 +1,9 @@
-import React, { PureComponent } from 'react';
+import React, { FunctionComponent, memo } from 'react';
 import { Text } from 'react-native';
 import FSI18n, { translationKeys } from '@brandingbrand/fsi18n';
 
-export default class EmptyCart extends PureComponent {
-  render(): JSX.Element {
-    return <Text>{FSI18n.string(translationKeys.flagship.cart.isEmpty)}</Text>;
-  }
-}
+const EmptyCart: FunctionComponent = () => {
+  return <Text>{FSI18n.string(translationKeys.flagship.cart.isEmpty)}</Text>;
+};
+
+export default memo(EmptyCart);
