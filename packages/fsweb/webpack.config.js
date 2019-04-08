c75573226ea543c16b317834d72edd19a6e80b5f
Brett Weissbart <brett@brandingbrand.com>
2019-04-08 16:26:29 -0400

    feat(fsweb): get pirateship working again on web
    This implements a few fixes to get PirateShip working on web again:
     - Add babel.config to fsweb (current version of Babel doesn't appear to read from .babelrc anymore)
     - Add uglify webpack plugin (was bundled with prior versions of Webepack)
     - Add __DEFAULT_ENV__ to dev webpack config, defaults to production if not provided to keep behavior consistent with native
     - Fix compile-web script in PirateShip to properly send platform argument to Flagship init

--- packages/fsweb/webpack.config.js
+++ packages/fsweb/webpack.config.js
@@ -173,7 +173,7 @@ module.exports = function(env, options) {
       }),
       new webpack.DefinePlugin({
         __DEV__: env.enableDev ? true : false,
-        __DEFAULT_ENV__: JSON.stringify(env.defaultEnvName)
+        __DEFAULT_ENV__: JSON.stringify((env && env.defaultEnvName) || 'prod')
       }),
       new UglifyJsPlugin({
         test: /.m?[jt]sx?/,
@@ -250,7 +250,8 @@ module.exports = function(env, options) {
         filename: 'static/css/bundle.css'
       }),
       new webpack.DefinePlugin({
-        __DEV__: true
+        __DEV__: true,
+        __DEFAULT_ENV__: JSON.stringify((env && env.defaultEnvName) || 'prod')
       })
     ]);
   }
