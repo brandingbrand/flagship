c5bf0f8b0492236a208849326137b0d26a670486
Brett Weissbart <brett@brandingbrand.com>
2019-05-17 11:51:22 -0400

    feat(flagship): refactor module scripts and remove node-suspect
    This makes the following changes to the module script system:
    - Adds ios and android directories under the modules directory in flagship
    - Updated the module system to support two lifecycle functions per module: preLink and postLink
    - Updated existing module scripts to replicate the same behavior as before this change: existing ios scripts run as preLink, and existing android scripts run as postLink
    
    The reason for this change is to support updating the react-native-codepush init process. Previously, code-push required its keys to be provided via CLI prompts during init, which required us to implement node-suspect to handle CLI interaction. node-suspect is buggy itself and this solution is hacky.
    
    react-native-codepush recently added support for providing keys in strings.xml (Android) and Info.plist (iOS). However, this requires these files to be modified before react-native link runs, and our implementation of Android module scripts had them run after link. As such, the previously mentioned changes were required in order to allow having an Android script run before rn link.
    
    This shouldn't break Flagship apps using react-native-codepush as it still pulls keys from the same configuration.

--- packages/flagship/src/lib/link.ts
+++ packages/flagship/src/lib/link.ts
@@ -1,55 +1,29 @@
-import { Config } from '../types';
-import { spawn } from 'node-suspect';
-import * as path from './path';
+import { spawn } from 'child_process';
 import * as helpers from '../helpers';
+import * as path from './path';
 import * as os from './os';
 
-/*
- * A set of platforms to link.
- * @typedef {Object} LinkPlatforms
- * @property {boolean} android - Whether or not to link for Android.
- * @property {boolean} ios - Whether or not to link for iOS.
- */
-
 /**
- * Runs `react-native link` on the project.
- * @param {object} The project configuration.
- * @returns {Promise<void>}
+ * Runs react-native link on the current project.
+ *
+ * @returns {Promise<void>} A promise representing the child process running react-native link.
  */
-export async function link(configuration: Config): Promise<boolean | object> {
-  helpers.logInfo(`running react-native link`);
+export async function link(): Promise<void> {
+  helpers.logInfo('running react-native link');
 
-  const androidDeploymentKey = configuration.codepush &&
-    configuration.codepush.android.deploymentKey;
-  const iosDeploymentKey = configuration.codepush &&
-    configuration.codepush.ios.deploymentKey;
-
-  // Spawn react-native link
-  const spawned = spawn('react-native', ['link'], {
-    cwd: path.project.path(),
-    shell: os.win
-  });
-
-  if (androidDeploymentKey && iosDeploymentKey) {
-    spawned
-      .wait('What is your CodePush deployment key for Android (hit <ENTER> to ignore)')
-      .sendline(androidDeploymentKey)
-      .wait('What is your CodePush deployment key for iOS (hit <ENTER> to ignore)')
-      .sendline(iosDeploymentKey)
-      .sendEof();
-  } else {
-    spawned.sendEof();
-  }
-
-  return new Promise<boolean | object>((resolve, reject) => {
-    return spawned.run((err: any) => {
-      if (!err) {
-        helpers.logInfo('react-native link finished successfully');
-        return resolve(true);
-      } else {
-        helpers.logError('react-native link could not complete', err);
-        return reject(err);
-      }
+  return new Promise<void>((resolve, reject) => {
+    const spawned = spawn('react-native', ['link'], {
+      cwd: path.project.path(),
+      shell: os.win
     });
+
+    // Redirect child process output to process stdout/stderr so we can see script output
+    spawned.stdout.pipe(process.stdout);
+    spawned.stderr.pipe(process.stderr);
+
+    spawned.on('error', e => reject(new Error('Error spawning react-native link' + e)));
+    spawned.on('close', resolve);
+
+    spawned.stdin.end();
   });
 }
