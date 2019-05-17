1836ea2fdc516f1c30679167f3b30b733fcc8053
Brett Weissbart <brett@brandingbrand.com>
2019-05-17 11:51:22 -0400

    feat(flagship): refactor module scripts and remove node-suspect
    This makes the following changes to the module script system:
    - Adds ios and android directories under the modules directory in flagship
    - Updated the module system to support two lifecycle functions per module: preLink and postLink
    - Updated existing module scripts to replicate the same behavior as before this change: existing ios scripts run as preLink, and existing android scripts run as postLink
    
    The reason for this change is to support updating the react-native-codepush init process. Previously, code-push required its keys to be provided via CLI prompts during init, which required us to implement node-suspect to handle CLI interaction. node-suspect is buggy itself and this solution is hacky.
    
    react-native-codepush recently added support for providing keys in strings.xml (Android) and Info.plist (iOS). However, this requires these files to be modified before react-native link runs, and our implementation of Android module scripts had them run after link. As such, the previously mentioned changes were required in order to allow having an Android script run before rn link.
    
    This shouldn't break Flagship apps using react-native-codepush as it still pulls keys from the same configuration.

--- packages/flagship/src/lib/modules/android/react-native-leanplum.ts
+++ packages/flagship/src/lib/modules/android/react-native-leanplum.ts
@@ -1,8 +1,7 @@
-import * as path from '../path';
-import * as fs from '../fs';
-import * as pods from '../cocoapods';
-import { Config } from '../../types';
-import { logInfo } from '../../helpers';
+import * as path from '../../path';
+import * as fs from '../../fs';
+import { Config } from '../../../types';
+import { logInfo } from '../../../helpers';
 
 const kRepository = `maven { url 'https://repo.leanplum.com/' }`;
 
@@ -11,7 +10,7 @@ const kRepository = `maven { url 'https://repo.leanplum.com/' }`;
  *
  * @param {object} configuration The project configuration.
  */
-export function android(configuration: Config): void {
+export function postLink(configuration: Config): void {
   logInfo('patching Android for react-native-leanplum');
 
   // Add the repository to the project repositories.
@@ -22,13 +21,4 @@ export function android(configuration: Config): void {
     'new RNLeanplumPackage()',
     'new RNLeanplumPackage(application)'
   );
-}
-
-/**
- * Patches iOS for the module.
- *
- * @param {object} configuration The project configuration.
- */
-export function ios(configuration: Config): void {
-  pods.add(path.ios.podfilePath(), [`pod "Leanplum-iOS-SDK", '2.1.0'`]);
 }
