4e496134e6bb16b932aec0d7a6a54e690db60f1f
Cauldrath <ben@brandingbrand.com>
2019-08-05 18:13:33 -0400

    fix(fsweb): prevent double transpiling of typeof
    Both metro-react-native-babel-preset and @babel/preset-env transpile "typeof", which causes it to
    include itself. This prevents the transpiling of typeof in @babel/preset-env. Also made the two
    babel configs work the same way
    
    brandingbrand-source-id: 78afe79f113f21c0ec7bf914c1bcc15d174e5ea3

--- packages/fsweb/babel.config.js
+++ packages/fsweb/babel.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   "presets": [
     "module:metro-react-native-babel-preset",
-    ["@babel/env", {
+    ["@babel/preset-env", {
+      "exclude": ["transform-typeof-symbol"],
+      "modules": false,
       "targets": {
         "browsers": ["last 2 versions", "ie >= 11"]
-      },
-      "modules": false
+      }
     }]
   ],
   "plugins": [
