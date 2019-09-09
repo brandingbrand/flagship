0cde81d45c64cc60d52da89b9b471b05b1fd5c5f
Nathan Sankbeil <nathan.sankbeil@brandingbrand.com>
2019-09-09 16:16:30 -0400

    fix: Add more stubbed configs

--- packages/create-fs-app/src/assets/base-config.ts
+++ packages/create-fs-app/src/assets/base-config.ts
@@ -22,6 +22,16 @@ export const baseConfig = {
     android: 'assets/appIcon/android',
     ios: 'assets/appIcon/ios'
   },
+  googleAnalytics: {
+    android: '',
+    ios: ''
+  },
+  engagement: {
+    baseURL: 'https://kong.uat.bbhosted.com/engagement/v1',
+    cacheTTL: 0,
+    appId: '302891af-778a-46aa-abdc-7297e72a1809',
+    apiKey: 'f3e1a902963f3364980b31e7ad0ea4'
+  },
   usageDescriptionIOS: {
     NSAppleMusicUsageDescription: '',
     NSBluetoothPeripheralUsageDescription: '',
