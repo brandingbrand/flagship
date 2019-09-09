c03cd6fbc8def908c3450071d4ff052a8a26ef22
Nathan Sankbeil <nathan.sankbeil@brandingbrand.com>
2019-09-09 17:27:35 -0400

    fix: Add check that script is running on node 10 or above

--- packages/create-fs-app/index.js
+++ packages/create-fs-app/index.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
+
+const currentNodeMajorVersion = process.versions.node.split('.')[0];
+
+if (currentNodeMajorVersion < 10) {
+  console.error('create-fs-app requires Node 10 or higher.\n');
+  process.exit(1);
+}
+
 require('./dist/index');
