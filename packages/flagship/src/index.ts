936d308f02210e3bf39234b61c784341aec040f7
Nathan Sankbeil <nathan.sankbeil@brandingbrand.com>
2019-09-24 11:03:18 -0400

    fix(flagship): make android namespace match package name
    The namespace for native android code was always set to
    com.brandingbrand.reactnative.and.{config.name}, rather than using the defined ID. This lead to some
    issues with starting activities and imports.
    
    BREAKING CHANGE: If anything hard codes the android native project path (ie.
    android/app/src/main/java/com/brandingbrand/reactnative/and/project-name), it will need to be
    updated to account for the dynamic package name. Flagship now exports its own modules, so
    `flagship.path.android.nativeProjectPath(config)` can be used to get the directory without hard
    coding a path.
    
    brandingbrand-source-id: 096f75ad552a368a1147db733d76f03b1a94b0e2

---
+++ packages/flagship/src/index.ts
@@ -0,0 +1,33 @@
+import * as android from './lib/android';
+import * as cocoapods from './lib/cocoapods';
+import * as deeplinking from './lib/deeplinking';
+import * as env from './lib/env';
+import * as fastlane from './lib/fastlane';
+import * as fs from './lib/fs';
+import * as ios from './lib/ios';
+import * as link from './lib/link';
+import * as modules from './lib/modules';
+import * as nativeConstants from './lib/native-constants';
+import * as os from './lib/os';
+import * as path from './lib/path';
+import * as rename from './lib/rename';
+import * as version from './lib/version';
+import * as web from './lib/web';
+
+export {
+  android,
+  cocoapods,
+  deeplinking,
+  env,
+  fastlane,
+  fs,
+  ios,
+  link,
+  modules,
+  nativeConstants,
+  os,
+  path,
+  rename,
+  version,
+  web
+};
