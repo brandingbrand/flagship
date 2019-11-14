4042961d8d15f4e2bc9ed85d63e52fed89035bd4
Cauldrath <ben@brandingbrand.com>
2019-11-14 11:26:15 -0500

    feat(flagship): Add option to only include the default env

--- packages/flagship/src/commands/init.ts
+++ packages/flagship/src/commands/init.ts
@@ -29,6 +29,7 @@ export interface BuilderArgs {
 export interface HandlerArgs {
   platform: string;
   env: string;
+  onlyDefault: boolean;
 }
 
 const TEMPLATE_ANDROID_PACKAGE = 'com.brandingbrand.reactnative.and.flagship';
@@ -59,7 +60,7 @@ export function handler(argv: HandlerArgs): void {
   helpers.logInfo(`Flagship ${platform} init`);
 
   const projectPackageJSON = require(path.project.resolve('package.json'));
-  const configuration = initEnvironment(argv.env, projectPackageJSON);
+  const configuration = initEnvironment(argv.env, projectPackageJSON, argv.onlyDefault);
 
   if (doAndroid) {
     initAndroid(projectPackageJSON, configuration, projectPackageJSON.version, argv.env);
@@ -100,12 +101,13 @@ export function handler(argv: HandlerArgs): void {
  */
 function initEnvironment(
   environmentIdentifier: string,
-  packageJSON: NPMPackageConfig
+  packageJSON: NPMPackageConfig,
+  onlyDefault?: boolean
 ): Config {
   const configuration = env.configuration(environmentIdentifier, packageJSON);
 
   env.write(configuration); // Replace env.js with the current environment
-  env.createEnvIndex();
+  env.createEnvIndex(onlyDefault ? environmentIdentifier : undefined);
 
   return configuration;
 }
