b58032a39ce5ef30f855dfc3eaf1e9c68a9c90bc
Andrew Schwob <andy.schwob@brandingbrand.com>
2019-12-17 14:06:27 -0500

    feat(fsengagement): add animated card list for demo (#954)
    feat(fsengagement): add animated card list

---
+++ packages/fsengagement/src/inboxblocks/WhiteInboxWrapper.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+  StyleProp, StyleSheet, TextStyle, View
+} from 'react-native';
+import { InjectedProps } from '../types';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff'
+  }
+});
+
+export interface CardProps extends InjectedProps {
+  containerStyle?: StyleProp<TextStyle>;
+}
+
+export default class WhiteInboxWrapper extends Component<CardProps> {
+  render(): JSX.Element {
+    return (
+      <View style={styles.container}>
+        {this.props.children}
+      </View>
+    );
+  }
+}
