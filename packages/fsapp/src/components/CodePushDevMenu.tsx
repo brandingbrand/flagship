46a748e328317d99055c779c5d2dbf169195e5f9
Cauldrath <ben@brandingbrand.com>
2020-01-02 17:11:31 -0500

    fix(fsapp): Update redux and react
    I was getting errors (unable to access the store when calling connect) with redux 4.0.4, which went away after upgrading to 4.0.5.

--- packages/fsapp/src/components/CodePushDevMenu.tsx
+++ packages/fsapp/src/components/CodePushDevMenu.tsx
@@ -140,7 +140,7 @@ export default class CodePushDevMenu extends Component<
 
   async componentDidMount(): Promise<void> {
     this.setState({
-      version: await DeviceInfo.getVersion()
+      version: DeviceInfo.getVersion()
     });
   }
 
