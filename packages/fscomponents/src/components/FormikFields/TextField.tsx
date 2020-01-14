f0635035cec9d137c8030b3d75046d1dea96ee59
Stefan Neamtu <stefan.neamtu@brandingbrand.com>
2020-01-14 17:41:54 -0500

    feat(fscomponents): add formik, yup to fscomponents and LoginForm
    brandingbrand-source-id: a962944d7bef733b8149991693e34977109dfb84

---
+++ packages/fscomponents/src/components/FormikFields/TextField.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useField, useFormikContext } from 'formik';
+import { StyleProp, Text, TextInput, TextInputProps, TextStyle, View } from 'react-native';
+import { style as S } from '../../styles/FormFK';
+
+
+interface FormFieldProps extends TextInputProps {
+  label?: string;
+  name: string;
+  labelStyle?: StyleProp<TextStyle>;
+  errStyle?: StyleProp<TextStyle>;
+}
+
+export const TextField: React.FC<FormFieldProps> = (
+  // tslint:disable-next-line:handle-callback-err
+  {label, name, labelStyle, errStyle, ...props}) => {
+  const [field, meta] = useField(name);
+  const { handleChange, setFieldTouched } = useFormikContext();
+  const fieldName = field.name;
+  const setTouched = () => setFieldTouched(fieldName);
+  return (
+    <View style={S.inputContainer}>
+      {label && <Text style={[S.label, labelStyle]}>{label}</Text>}
+        <TextInput
+          {...props}
+          onChangeText={handleChange(fieldName)}
+          onBlur={setTouched}
+        />
+      {meta.touched && meta.error ?
+      <Text style={[S.errorMessageText, errStyle]}>
+        {meta.error}
+      </Text> : null}
+    </View>
+  );
+};
