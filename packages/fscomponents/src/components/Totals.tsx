9e4de98415c221299c4d62be3969ee2083709a5c
Huizhu Zhang <huizhu.zhang@brandingbrand.com>
2020-01-17 15:45:25 -0500

    refactor(fscomponents): FLAGSHIP-70 update to function component

--- packages/fscomponents/src/components/Totals.tsx
+++ packages/fscomponents/src/components/Totals.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleProp, View, ViewStyle } from 'react-native';
 
 import { Total, TotalProps } from './Total';
@@ -8,21 +8,19 @@ export interface TotalsProps {
   style?: StyleProp<ViewStyle>;
 }
 
-export class Totals extends Component<TotalsProps> {
-  render(): JSX.Element {
-    const { style, totals } = this.props;
+export const Totals = (props: TotalsProps): JSX.Element => {
+  const { style, totals } = props;
 
-    return (
-      <View style={style}>
-        {totals.map((total: TotalProps, index: number) => {
-          return (
-            <Total
-              key={typeof total.keyName === 'string' ? total.keyName : index}
-              {...total}
-            />
-          );
-        })}
-      </View>
-    );
-  }
-}
+  return (
+    <View style={style}>
+      {totals.map((total: TotalProps, index: number) => {
+        return (
+          <Total
+            key={typeof total.keyName === 'string' ? total.keyName : index}
+            {...total}
+          />
+        );
+      })}
+    </View>
+  );
+};
