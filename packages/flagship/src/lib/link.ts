dbea4a39e568d03a7c7fbdce5da790e8da6eb11d
Cauldrath <ben@brandingbrand.com>
2020-01-17 16:01:34 -0500

    fix(flagship): Run react-native link to link assets
    Assets like fonts need to be linked so the application will have access to them.

--- packages/flagship/src/lib/link.ts
+++ packages/flagship/src/lib/link.ts
@@ -3,6 +3,26 @@ import * as helpers from '../helpers';
 import * as path from './path';
 import * as os from './os';
 
+async function runLink(name?: string): Promise<void> {
+  return new Promise<void>((packageResolve, packageReject) => {
+    const spawned = spawn('react-native', name ? ['link', name] : ['link'], {
+      cwd: path.project.path(),
+      shell: os.win
+    });
+
+    // Redirect child process output to process stdout/stderr so we can see script output
+    spawned.stdout.pipe(process.stdout);
+    spawned.stderr.pipe(process.stderr);
+
+    spawned.on('error', e => packageReject(
+      new Error('Error spawning react-native link' + e))
+    );
+    spawned.on('close', packageResolve);
+
+    spawned.stdin.end();
+  });
+}
+
 /**
  * Runs react-native link on the current project.
  *
@@ -16,25 +36,11 @@ export async function link(forceLink?: string[]): Promise<void> {
         if (forceLink.hasOwnProperty(name)) {
           helpers.logInfo('running react-native link for ' + [forceLink[name]]);
 
-          await new Promise<void>((packageResolve, packageReject) => {
-            const spawned = spawn('react-native', ['link', forceLink[name]], {
-              cwd: path.project.path(),
-              shell: os.win
-            });
-
-            // Redirect child process output to process stdout/stderr so we can see script output
-            spawned.stdout.pipe(process.stdout);
-            spawned.stderr.pipe(process.stderr);
-
-            spawned.on('error', e => packageReject(
-              new Error('Error spawning react-native link' + e))
-            );
-            spawned.on('close', packageResolve);
-
-            spawned.stdin.end();
-          });
+          await runLink(forceLink[name]);
         }
       }
+      helpers.logInfo('running react-native link to link assets');
+      await runLink();
       resolve();
     });
   }
