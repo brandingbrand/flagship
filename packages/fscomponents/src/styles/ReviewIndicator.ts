e3b2531a45d8675da2122701a73bbd55806f9076
KO-HSIN CHU <kohsin.chu@brandingbrand.com>
2020-01-24 16:53:50 -0500

    fix(fscomponents): replace Arial with sans-serif
    brandingbrand-source-id: 4b3e8cc9dfbd5dfe9bffa50dababdffe3920fce7

--- packages/fscomponents/src/styles/ReviewIndicator.ts
+++ packages/fscomponents/src/styles/ReviewIndicator.ts
@@ -17,7 +17,6 @@ export const style = StyleSheet.create({
     width: 18,
     color: '#333132',
     textAlign: 'center',
-    fontFamily: 'Arial',
     // includeFontPadding is not a valid property for web/ios
     ...Platform.select({
       android: {
