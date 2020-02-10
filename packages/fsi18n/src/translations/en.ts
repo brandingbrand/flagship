9d7d550a78112267d07bedad82b78c985df8d7b8
willybeans <will.wedmedyk@brandingbrand.com>
2020-02-10 14:55:12 -0500

    feat(fscomponents): add translations for EmailForm
    brandingbrand-source-id: 7f383ba68ccf1c36b1deb08e087a82ba64fc50f5

--- packages/fsi18n/src/translations/en.ts
+++ packages/fsi18n/src/translations/en.ts
@@ -327,6 +327,10 @@ export const keys: FSTranslationKeys = {
       password: 'Password',
       passwordError: 'Please enter your password',
       submit: 'Submit'
+    },
+    emailForm: {
+      placeholder: 'Email',
+      error: 'Required Field'
     }
   }
 };
