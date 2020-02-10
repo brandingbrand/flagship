aee21f1846781490c12cec649a5ad0e34683248f
willybeans <will.wedmedyk@brandingbrand.com>
2020-02-10 16:00:38 -0500

    feat(fscomponents): add translations for AddToCart
    brandingbrand-source-id: 41cff9297816d2051b0bf9eaba92dc40bca79fa3

--- packages/fsi18n/src/types.ts
+++ packages/fsi18n/src/types.ts
@@ -100,6 +100,7 @@ export interface FilterListDefaultTranslations<KeyType = TranslationKey> {
 export interface CartTranslations<KeyType> {
   isEmpty: KeyType;
   itemsInCart: KeyType;
+  error: KeyType;
   actions: {
     add: {
       actionBtn: KeyType;
