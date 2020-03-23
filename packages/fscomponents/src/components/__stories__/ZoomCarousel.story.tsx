dce1b37fff4634edc46748d8d99d32bcbdeff82c
Luke Henze <luke.henze@brandingbrand.com>
2020-03-23 16:37:05 -0400

    feat(fscomponents): Add knobs to zoom carousel story

--- packages/fscomponents/src/components/__stories__/ZoomCarousel.story.tsx
+++ packages/fscomponents/src/components/__stories__/ZoomCarousel.story.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react'; // tslint:disable-line:no-implicit-dependencies
 import { ZoomCarousel } from '../ZoomCarousel/ZoomCarousel';
+// tslint:disable-next-line:no-implicit-dependencies
+import { boolean, number, object } from '@storybook/addon-knobs';
 
-const images = [
+const defaultImages = [
   {
     src: require('./assets/images/wide.png'),
     zoomSrc: require('./assets/images/wide@2x.png')
@@ -24,11 +26,13 @@ const images = [
 storiesOf('ZoomCarousel', module)
   .add('basic usage', () => (
     <ZoomCarousel
-      peekSize={20}
-      gapSize={10}
-      centerMode={true}
-      showThumbnails={true}
-      images={images}
-      showArrow={true}
+      images={object('Images', defaultImages)}
+      peekSize={number('Peek Size', 20)}
+      gapSize={number('Gap Size', 10)}
+      centerMode={boolean('Center Mode', true)}
+      showThumbnails={boolean('Show Thumbnails', true)}
+      showArrow={boolean('Show Arrow', true)}
+      hideZoomButton={boolean('Hide Zoom Button', false)}
+      fillContainer={boolean('Fill Container', false)}
     />
   ));
