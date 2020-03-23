830dd84f1d5e229e0558815886916d679f2ebe10
Stefan Neamtu <stefan.neamtu@brandingbrand.com>
2020-03-23 15:39:02 -0400

    feat(fscomponents): add image counter, new style options to ZoomCarousel
    - Added a new option that can be rendered: image counter
     - Absolutely placed component with custom rendering or custom styles as props
     - Added new prop options for the carousel and image containers
     - changed the default container styling to be flex friendly, fixing an issue where thumbnails would
       overflow beyond parent boundaries
    
    brandingbrand-source-id: 38ed6ada4fdce4e14e19e092476cdfcb7f3b0011

--- packages/fscomponents/src/components/ZoomCarousel/ZoomCarousel.web.tsx
+++ packages/fscomponents/src/components/ZoomCarousel/ZoomCarousel.web.tsx
@@ -11,6 +11,7 @@ import {
   Image,
   ScrollView,
   StyleSheet,
+  Text,
   TouchableOpacity,
   View
 } from 'react-native';
@@ -51,6 +52,10 @@ export interface ZoomCarouselStateType {
 }
 
 const S = StyleSheet.create({
+  carouselContainer: {
+    flex: 1,
+    flexBasis: 'auto'
+  },
   searchIcon: {
     width: 25,
     height: 25
@@ -107,9 +112,6 @@ const S = StyleSheet.create({
   fullHeight: {
     height: '100%'
   },
-  fullHeightWithThumbnails: {
-    height: '85%'
-  },
   thumbnailImg: {
     width: '100%',
     height: '100%'
@@ -125,6 +127,11 @@ const S = StyleSheet.create({
   thumbnailSelected: {
     borderWidth: 3,
     borderColor: 'red'
+  },
+  imageCounter: {
+    position: 'absolute',
+    right: 0,
+    top: 0
   }
 });
 
@@ -266,6 +273,19 @@ export class ZoomCarousel extends Component<ZoomCarouselProps, ZoomCarouselState
     );
   }
 
+  renderImageCounter = () => {
+    const total: number = this.props.images && this.props.images.length || 0;
+    const currentIndex = this.state.currentIndex + 1;
+
+    return (
+      <View style={this.props.imageCounterStyle || S.imageCounter}>
+        <Text>
+          {`${currentIndex}/${total}`}
+        </Text>
+      </View>
+    );
+  }
+
   renderPhotoSwipe = () => (
     <PhotoSwipe
       isOpen={this.state.isZooming}
@@ -304,20 +324,46 @@ export class ZoomCarousel extends Component<ZoomCarouselProps, ZoomCarouselState
     this.renderPhotoSwipe()
   )
 
+  renderThumbnails = () => (
+    <ScrollView
+      horizontal={true}
+      contentContainerStyle={[
+        S.thumbnailContainer,
+        this.props.thumbnailContainerStyle
+      ]}
+    >
+    {this.props.images.map((img, i) => (
+      <TouchableOpacity
+        key={i}
+        style={[
+          S.thumbnail,
+          this.props.thumbnailStyle,
+          this.state.currentIndex === i && S.thumbnailSelected
+        ]}
+        onPress={this.handleThumbPress(i)}
+        accessibilityRole={'button'}
+        accessibilityLabel={FSI18n.string(componentTranslationKeys.focus.actionBtn)}
+      >
+        <Image
+          resizeMode='cover'
+          source={img.src}
+          style={S.thumbnailImg}
+        />
+      </TouchableOpacity>
+    ))}
+    </ScrollView>
+  )
+
+  // tslint:disable-next-line:cyclomatic-complexity
   render(): JSX.Element {
     const { peekSize = 0, gapSize = 0 } = this.props;
-    const height = this.props.fillContainer ? this.props.fillContainerStyle ||
-      (this.props.showThumbnails ? S.fullHeightWithThumbnails : S.fullHeight)
-       : null;
-
-    // line 316 and 324 needs to be at 100% height
     return (
       <View
-        style={S.fullHeight}
+        style={this.props.contentContainerStyle || S.carouselContainer}
         onLayout={this.handleLayoutChange}
       >
         <View
-          style={height}
+          style={this.props.imageContainerStyle || S.carouselContainer}
         >
           <div
             id={`zoom-carousel-${this.id}`}
@@ -337,7 +383,7 @@ export class ZoomCarousel extends Component<ZoomCarouselProps, ZoomCarouselState
               zoomButtonStyle={this.props.zoomButtonStyle}
               renderPageIndicator={this.props.renderPageIndicator}
               centerMode={this.props.centerMode}
-              style={height}
+              style={this.props.fillContainer ? S.fullHeight : null}
               nextArrowOnBlur={this.props.nextArrowOnBlur}
               hidePageIndicator={this.props.hidePageIndicator}
             />
@@ -365,34 +411,13 @@ export class ZoomCarousel extends Component<ZoomCarouselProps, ZoomCarouselState
           (this.props.renderThumbnails ? (
             this.props.renderThumbnails(this.state.currentIndex, this.goTo)
           ) : (
-            <ScrollView
-              horizontal={true}
-              contentContainerStyle={[
-                S.thumbnailContainer,
-                this.props.thumbnailContainerStyle
-              ]}
-            >
-              {this.props.images.map((img, i) => (
-                <TouchableOpacity
-                  key={i}
-                  style={[
-                    S.thumbnail,
-                    this.props.thumbnailStyle,
-                    this.state.currentIndex === i && S.thumbnailSelected
-                  ]}
-                  onPress={this.handleThumbPress(i)}
-                  accessibilityRole={'button'}
-                  accessibilityLabel={FSI18n.string(componentTranslationKeys.focus.actionBtn)}
-                >
-                  <Image
-                    resizeMode='cover'
-                    source={img.src}
-                    style={S.thumbnailImg}
-                  />
-                </TouchableOpacity>
-              ))}
-            </ScrollView>
+            this.renderThumbnails()
           ))}
+          {this.props.showImageCounter &&
+          (this.props.renderImageCounter ?
+            this.props.renderImageCounter(this.state.currentIndex) :
+            (this.renderImageCounter())
+          )}
       </View>
     );
   }
