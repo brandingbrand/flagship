b1d26bdcdce73e589e1d814c062f6e1f885fb167
Luke Henze <lhenze3@gmail.com>
2020-03-26 15:05:48 -0400

    feat(fscomponents): add knobs to zoom carousel
    brandingbrand-source-id: 7a2e22ad4cef0a1b48d901bf1944c5ccaa519dae

--- packages/fscomponents/src/components/__stories__/ZoomCarousel.story.tsx
+++ packages/fscomponents/src/components/__stories__/ZoomCarousel.story.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react'; // tslint:disable-line:no-implicit-dependencies
 import { ZoomCarousel } from '../ZoomCarousel/ZoomCarousel';
+// tslint:disable-next-line:no-implicit-dependencies
+import { boolean, number, object } from '@storybook/addon-knobs';
 
-const images = [
+const defaultImages = [
   {
     src: require('./assets/images/wide.png'),
     zoomSrc: require('./assets/images/wide@2x.png')
@@ -24,31 +26,35 @@ const images = [
 storiesOf('ZoomCarousel', module)
   .add('basic usage', () => (
       <ZoomCarousel
-        peekSize={20}
-        gapSize={10}
-        centerMode={true}
-        images={images}
-        showArrow={true}
+        images={object('Images', defaultImages)}
+        peekSize={number('Peek Size', 20)}
+        gapSize={number('Gap Size', 10)}
+        centerMode={boolean('Center Mode', true)}
+        showArrow={boolean('Show Arrow', true)}
+        hideZoomButton={boolean('Hide Zoom Button', false)}
+        fillContainer={boolean('Fill Container', false)}
       />
   )).add('with thumbnails', () => (
       <ZoomCarousel
-        peekSize={20}
-        gapSize={10}
-        centerMode={true}
-        showThumbnails={true}
-        images={images}
-        showArrow={true}
-        fillContainer={true}
+        images={object('Images', defaultImages)}
+        peekSize={number('Peek Size', 20)}
+        gapSize={number('Gap Size', 10)}
+        centerMode={boolean('Center Mode', true)}
+        showArrow={boolean('Show Arrow', true)}
+        hideZoomButton={boolean('Hide Zoom Button', false)}
+        fillContainer={boolean('Fill Container', false)}
+        showThumbnails={boolean('Show Thumbnails', true)}
       />
   )).add('with image counter', () => (
     <ZoomCarousel
-      peekSize={20}
-      gapSize={10}
-      centerMode={true}
-      showThumbnails={true}
-      images={images}
-      showArrow={true}
-      fillContainer={true}
-      showImageCounter={true}
+      images={object('Images', defaultImages)}
+      peekSize={number('Peek Size', 20)}
+      gapSize={number('Gap Size', 10)}
+      centerMode={boolean('Center Mode', true)}
+      showArrow={boolean('Show Arrow', true)}
+      hideZoomButton={boolean('Hide Zoom Button', false)}
+      fillContainer={boolean('Fill Container', false)}
+      showThumbnails={boolean('Show Thumbnails', true)}
+      showImageCounter={boolean('Show Image Counter', true)}
     />
 ));
