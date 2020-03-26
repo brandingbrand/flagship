67b7c1a3908a90562dcfa92d7652638e7368407e
ivanmatchyn <ivan.matchyn@metroglobal.net>
2020-03-26 18:44:43 +0200

    feat: add story-knobs to fadeInImage component

--- packages/fscomponents/src/components/__stories__/FadeInImage.story.tsx
+++ packages/fscomponents/src/components/__stories__/FadeInImage.story.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {
+  Linking,
   StyleSheet,
-  View
+  TouchableOpacity
 } from 'react-native';
 import { storiesOf } from '@storybook/react'; // tslint:disable-line:no-implicit-dependencies
-import { FadeInImage } from '../FadeInImage/FadeInImage';
+import { FadeInImage } from '../FadeInImage';
 import {
-  files
+  files, text
 // tslint:disable-next-line no-submodule-imports no-implicit-dependencies
 } from '@storybook/addon-knobs/react';
+
 const styles = StyleSheet.create({
   imageStyle: {
     width: 100,
@@ -20,20 +22,25 @@ const styles = StyleSheet.create({
   }
 });
 
+const deepLink = (deepLinkText: any) => () => {
+  Linking.openURL(deepLinkText);
+};
+
 storiesOf('FadeInImage', module)
     .add('basic usage', () => {
+      const deepLinkText = text('Deep Link Url', 'https://google.com');
       const label = 'Images';
       const accept = '.xlsx, .pdf, .png, .jpg, jpeg';
       const defaultValue = ['https://placehold.it/100x100'];
       const value = files(label, accept, defaultValue);
       return (
-          <View style={styles.imageContainer}>
+          <TouchableOpacity onPress={deepLink(deepLinkText)} style={styles.imageContainer}>
             <FadeInImage
                 source={{uri: value[0]}}
                 resizeMode='contain'
                 resizeMethod='resize'
                 style={styles.imageStyle}
             />
-          </View>
+          </TouchableOpacity>
       );
     });
