8121fda592a113367e9c59fbffb35b8bf464b7d0
Luke Henze <luke.henze@brandingbrand.com>
2020-04-13 16:30:14 -0400

    feat: add knobs to accordian story

--- packages/fscomponents/src/components/__stories__/Accordion.story.tsx
+++ packages/fscomponents/src/components/__stories__/Accordion.story.tsx
@@ -10,56 +10,83 @@ import {
 } from 'react-native';
 import { storiesOf } from '@storybook/react'; // tslint:disable-line:no-implicit-dependencies
 import { Accordion } from '../Accordion';
-
-const title = <Text>Menu Item</Text>;
+// tslint:disable-next-line:no-implicit-dependencies
+import { boolean, number, object, select, text } from '@storybook/addon-knobs';
 
 const icons = {
   closed: require('../../../assets/images/alert.png'),
   open: require('../../../assets/images/checkmarkValidation.png')
 };
 
-const content = (
-  <View>
-    <Text>Sub Menu Item</Text>
-  </View>
-);
+const renderTitle = (): JSX.Element => {
+  return (
+    <Text>{text('title', 'Menu Item')}</Text>
+  );
+};
 
-const imageContent = (
-  <View>
-    <Text>Sub Menu Item</Text>
-    <Image
-      resizeMode='contain'
-      source={{ uri: 'https://via.placeholder.com/350x150' }}
-      style={{ height: 150, width: 350 }}
-    />
-  </View>
-);
+const renderContent = (): JSX.Element => {
+  return(
+    <View>
+      <Text>Sub Menu Item</Text>
+    </View>
+  );
+};
+
+const renderImageContent = (): JSX.Element => {
+  const options = ['cover', 'contain', 'stretch', 'repeat', 'center'];
+  return (
+    <View>
+      <Text>Sub Menu Item</Text>
+      <Image
+        source={{ uri: text('Image Source', 'https://via.placeholder.com/350x150') }}
+        resizeMode={select('Image Resize Mode', options, 'cover') as any}
+        style={object('Image Style', { height: 150, width: 350 })}
+      />
+    </View>
+  );
+};
 
 storiesOf('Accordion', module)
   .add('basic usage', () => (
     <Accordion
-      title={title}
-      content={content}
+      title={renderTitle()}
+      style={object('style', {padding: 10})}
+      paddingHorizontal={number('paddingHorizontal?', 15)}
+      titleUnderlayColor={text('titleUnderlayColor?', 'transparent')}
+      disableAnimation={boolean('disableAnimation?', false)}
+      content={renderContent()}
     />
   ))
   .add('w/ image', () => (
     <Accordion
-      title={title}
-      content={imageContent}
+      title={renderTitle()}
+      style={object('style', {padding: 10})}
+      paddingHorizontal={number('paddingHorizontal?', 15)}
+      titleUnderlayColor={text('titleUnderlayColor?', 'transparent')}
+      disableAnimation={boolean('disableAnimation?', false)}
+      content={renderImageContent()}
     />
   ))
   .add('w/ arrow disclosure icon', () => (
     <Accordion
-      title={title}
-      content={content}
-      iconFormat={'arrow'}
+      title={renderTitle()}
+      style={object('style', {padding: 10})}
+      paddingHorizontal={number('paddingHorizontal?', 15)}
+      titleUnderlayColor={text('titleUnderlayColor?', 'transparent')}
+      content={renderContent()}
+      iconFormat={select('iconFormat?', ['plusminus', 'arrow'], 'arrow')}
+      disableAnimation={boolean('disableAnimation?', false)}
     />
   ))
   .add('w/ custom disclosure icon', () => (
     <Accordion
-      title={title}
-      content={content}
-      iconFormat={'image'}
+      title={renderTitle()}
+      style={object('style', {padding: 10})}
+      paddingHorizontal={number('paddingHorizontal?', 15)}
+      titleUnderlayColor={text('titleUnderlayColor?', 'transparent')}
+      content={renderContent()}
+      iconFormat={select('iconFormat?', ['image', 'plusminus', 'arrow'], 'image')}
+      disableAnimation={boolean('disableAnimation?', false)}
       openIconImage={icons.open}
       closedIconImage={icons.closed}
     />
