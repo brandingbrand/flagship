b22371b23c0fae6e6596bcc98bced4bf1bd93459
Luke Henze <luke.henze@brandingbrand.com>
2020-04-14 10:03:22 -0400

    feat(fscomponents): add knobs to accordian stories

--- packages/fscomponents/src/components/__stories__/Accordion.story.tsx
+++ packages/fscomponents/src/components/__stories__/Accordion.story.tsx
@@ -10,14 +10,15 @@ import {
   View
 } from 'react-native';
 import {
+  boolean,
+  number,
+  object,
   select,
   text
 // tslint:disable-next-line no-implicit-dependencies
 } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react'; // tslint:disable-line:no-implicit-dependencies
 import { Accordion } from '../Accordion';
-
-const title = <Text>Menu Item</Text>;
 
 const icons: Record<string, ImageURISource> = {
   closed: require('../../../assets/images/alert.png'),
@@ -30,42 +31,61 @@ const content = (
   </View>
 );
 
-const imageContent = (
-  <View>
-    <Text>Sub Menu Item</Text>
-    <Image
-      resizeMode='contain'
-      source={{ uri: 'https://via.placeholder.com/350x150' }}
-      style={{ height: 150, width: 350 }}
-    />
-  </View>
-);
+const renderImageContent = (): JSX.Element => {
+  const options = ['cover', 'contain', 'stretch', 'repeat', 'center'];
+  return (
+    <View>
+      <Text>Sub Menu Item</Text>
+      <Image
+        source={{ uri: text('Image Source', 'https://via.placeholder.com/350x150') }}
+        resizeMode={select('Image Resize Mode', options, 'cover') as any}
+        style={object('Image Style', { height: 150, width: 350 })}
+      />
+    </View>
+  );
+};
 
 storiesOf('Accordion', module)
   .add('basic usage', () => (
     <Accordion
-      title={title}
+      title={text('title', 'Menu Item')}
+      style={object('style', {padding: 10})}
+      paddingHorizontal={number('paddingHorizontal?', 15)}
+      titleUnderlayColor={text('titleUnderlayColor?', 'transparent')}
+      disableAnimation={boolean('disableAnimation?', false)}
       content={content}
     />
   ))
   .add('w/ image', () => (
     <Accordion
-      title={title}
-      content={imageContent}
+      title={text('title', 'Menu Item')}
+      style={object('style', {padding: 10})}
+      paddingHorizontal={number('paddingHorizontal?', 15)}
+      titleUnderlayColor={text('titleUnderlayColor?', 'transparent')}
+      disableAnimation={boolean('disableAnimation?', false)}
+      content={renderImageContent()}
     />
   ))
   .add('w/ arrow or plusminus disclosure icon', () => (
     <Accordion
-      title={title}
+      title={text('title', 'Menu Item')}
+      style={object('style', {padding: 10})}
+      paddingHorizontal={number('paddingHorizontal?', 15)}
+      titleUnderlayColor={text('titleUnderlayColor?', 'transparent')}
       content={content}
-      iconFormat={select('Format', ['arrow', 'plusminus'], 'arrow')}
+      iconFormat={select('iconFormat?', ['plusminus', 'arrow'], 'arrow')}
+      disableAnimation={boolean('disableAnimation?', false)}
     />
   ))
   .add('w/ custom disclosure icon', () => (
     <Accordion
-      title={title}
+      title={text('title', 'Menu Item')}
+      style={object('style', {padding: 10})}
+      paddingHorizontal={number('paddingHorizontal?', 15)}
+      titleUnderlayColor={text('titleUnderlayColor?', 'transparent')}
       content={content}
       iconFormat={'image'}
+      disableAnimation={boolean('disableAnimation?', false)}
       openIconImage={icons[select('Open Icon', Object.keys(icons), 'open')]}
       closedIconImage={icons[select('Closed Icon', Object.keys(icons), 'closed')]}
     />
@@ -73,12 +93,17 @@ storiesOf('Accordion', module)
   .add('nested as a child with a string title', () => (
     <Accordion
       title={text('Parent title', 'Parent')}
+      style={object('Parent style', {padding: 5})}
+      paddingHorizontal={number('Parent paddingHorizontal?', 0)}
+      titleUnderlayColor={text('Parent titleUnderlayColor?', 'transparent')}
       state={'open'}
     >
       <Accordion
-        title={title}
+        title={text('Child title', 'Menu Item')}
+        style={object('Child style', {padding: 5})}
+        paddingHorizontal={number('Child paddingHorizontal?', 0)}
+        titleUnderlayColor={text('Child titleUnderlayColor?', 'transparent')}
         content={content}
-        paddingHorizontal={0}
       />
     </Accordion>
   ));
