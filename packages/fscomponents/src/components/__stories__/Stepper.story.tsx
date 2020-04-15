d5f0dc8791dc02e8fb701da4d1e8efc7f6aa2327
romaniukua <romaniuk1511@gmail.com>
2020-04-15 13:06:35 +0300

    chore(fscomponents): add knobs for stepper story
    brandingbrand-source-id: eb7c978894045f10587a8623b9147a98865a47c3

--- packages/fscomponents/src/components/__stories__/Stepper.story.tsx
+++ packages/fscomponents/src/components/__stories__/Stepper.story.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'; // tslint:disable-line:no-implicit-dependencies
 import { action } from '@storybook/addon-actions'; // tslint:disable-line:no-implicit-dependencies
 import {
-  select
+  boolean,
+  number,
+  object,
+  select,
+  text
 // tslint:disable-next-line no-implicit-dependencies
 } from '@storybook/addon-knobs';
 import { Stepper } from '../Stepper';
@@ -13,12 +17,22 @@ const formats = [
   'vertical'
 ];
 
+const defaultCounterStyle = {
+  color: '#000',
+  marginRight: 10,
+  marginLeft: 10,
+  fontSize: 16
+};
+
 storiesOf('Stepper', module)
   .add('basic usage', () => (
     <Stepper
       format={select('format', formats, 'horizontalCenter') as any}
       count={2}
-      countUpperLimit={10}
+      counterStyle={object('defaultCounterStyle', defaultCounterStyle)}
+      editable={boolean('editable', false)}
+      prefix={text('prefix', '')}
+      countUpperLimit={number('countUpperLimit', 10)}
       onDecreaseButtonPress={action('Stepper onDecreaseButtonPress')}
       onIncreaseButtonPress={action('Stepper onIncreaseButtonPress')}
     />
