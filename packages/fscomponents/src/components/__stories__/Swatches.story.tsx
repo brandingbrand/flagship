d5014b21377f20fa247ec0ab107f973f950ff200
rosahbruno <rosahbruno@gmail.com>
2020-04-15 18:15:27 -0400

    chore(fscomponents): add knobs for swatches story
    Added some different knobs to the Swatches story.
    
    brandingbrand-source-id: 1a79421cfb943b9bac6c3573f24941fb476156f9

--- packages/fscomponents/src/components/__stories__/Swatches.story.tsx
+++ packages/fscomponents/src/components/__stories__/Swatches.story.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'; // tslint:disable-line:no-implicit-dependencies
 import { action } from '@storybook/addon-actions'; // tslint:disable-line:no-implicit-dependencies
 import {
-  boolean
+  boolean,
+  number,
+  object,
+  text
 // tslint:disable-next-line no-implicit-dependencies
 } from '@storybook/addon-knobs';
 import { Swatches } from '../Swatches';
 
+const containerStyle = { padding: 0 };
+const labelStyle = { padding: 0 };
+const showMoreLess = false;
+const swatchSize = 25;
+const maxSwatches = 6;
+const title = 'Selected';
 const items = [
   { color: 'red', name: 'red' },
   { color: 'green', name: 'green' },
@@ -22,8 +31,17 @@ const items = [
 storiesOf('Swatches', module)
   .add('basic usage', () => (
     <Swatches
-      items={items as any}
       label={boolean('label', true)}
+      title={text('title', title)}
+      moreLessStyle={{ display: boolean('showMoreLess', showMoreLess) ? 'flex' : 'none'}}
+      maxSwatches={number('maxSwatches', maxSwatches)}
+      colorContainerStyle={{
+        height: number('swatchSize', swatchSize),
+        width: number('swatchSize', swatchSize)
+      }}
+      labelTitleStyle={object('labelStyle', labelStyle)}
+      style={object('containerStyle', containerStyle)}
+      items={items as any}
       onChangeSwatch={action('Swatches onChangeSwatch')}
     />
   ));
