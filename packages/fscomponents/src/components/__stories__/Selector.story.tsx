e9cdac1fefeee3372be4f2d2c131ce15eb00e1da
romaniukua <romaniuk1511@gmail.com>
2020-04-15 16:18:49 +0300

    chore(fscomponents): add knobs for Selector story
    brandingbrand-source-id: 4c3ae6d5085b9b4f73030dc72bf17f088565a03d

--- packages/fscomponents/src/components/__stories__/Selector.story.tsx
+++ packages/fscomponents/src/components/__stories__/Selector.story.tsx
@@ -2,22 +2,28 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'; // tslint:disable-line:no-implicit-dependencies
 import { action } from '@storybook/addon-actions'; // tslint:disable-line:no-implicit-dependencies
 import {
+  number,
+  object,
   text
 // tslint:disable-next-line no-implicit-dependencies
 } from '@storybook/addon-knobs';
-import { Selector } from '../Selector/Selector';
+import { Selector } from '../Selector';
 
-const testItems = [...Array(10)].map((x, i) => ({
-  label: 'Product ' + i,
-  value: 'id-' + i
-}));
-
+const defaultStyle = {
+  padding: 15
+};
 storiesOf('Selector', module)
-  .add('basic usage', () => (
-    <Selector
-      style={{ padding: 15 }}
-      items={testItems}
-      title={text('title', 'Select Product')}
-      onValueChange={action('Selector onValueChange')}
-    />
-  ));
+  .add('basic usage', () => {
+    const testItems = [...Array(number('itemsCount', 10))].map((x, i) => ({
+      label: `Product ${i + 1}`,
+      value: `id-${i + 1}`
+    }));
+    return (
+      <Selector
+          style={object('defaultStyle', defaultStyle)}
+          items={testItems}
+          title={text('title', 'Select Product')}
+          onValueChange={action('Selector onValueChange')}
+      />
+    );
+  });
