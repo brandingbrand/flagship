05bece36d756bffd09569c62f5479bdf1537dfb7
romaniukua <romaniuk1511@gmail.com>
2020-04-17 17:07:54 +0300

    feat: implement firebase analytics provider

--- packages/fsengage/src/modules/analytics/providers/index.ts
+++ packages/fsengage/src/modules/analytics/providers/index.ts
@@ -3,4 +3,5 @@ export { default as AnalyticsProvider } from './AnalyticsProvider';
 export { default as AdobeMarketingCloudProvider } from './adobe/AdobeMarketingCloudProvider';
 export { default as GoogleAnalyticsProvider } from './google/GoogleAnalyticsProvider';
 export { default as LeanplumProvider } from './leanplum/LeanplumProvider';
+export { default as FirebaseAnalyticsProvider } from './firebase/FirebaseProvider';
 export * from './adobe/AdobeAnalyticsProvider';
