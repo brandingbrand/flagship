4785a200ac5733e03eed240d37b653ca008abedb
romaniukua <romaniuk1511@gmail.com>
2020-04-28 16:46:36 +0300

    feat: Replace all occurrences of "any" in FSTurnTo

--- packages/fsturnto/src/TurnToNormalizer.ts
+++ packages/fsturnto/src/TurnToNormalizer.ts
@@ -2,14 +2,54 @@ import {
   ReviewTypes
 } from '@brandingbrand/fscommerce';
 
-function review(ttReview: any): ReviewTypes.Review {
+export interface ReviewUser {
+  isStaffReviewer?: boolean;
+  isVerifiedBuyer?: boolean;
+  isVerifiedReviewer?: boolean;
+  location?: string;
+  name?: string;
+}
+
+export interface DimensionReview {
+  dimensionId: string;
+  dimensionLabel: string;
+  value: string;
+  label: string;
+}
+export interface DimensionSummary {
+  id: string;
+  label: string;
+  average: number;
+}
+
+export interface TTReview {
+  title: string;
+  text?: string;
+  rating: number;
+  user: {
+    nickName: ReviewUser;
+  };
+  dateCreated: string;
+  dimensions: DimensionReview[];
+  photos: [];
+}
+
+export interface TTSummary {
+  sku: string;
+  averageRating: number;
+  reviews: number;
+  ratingBreakdown: {[key: string] : number};
+  dimensions: DimensionSummary[];
+}
+
+function review<T extends TTReview>(ttReview: T): ReviewTypes.Review {
   return {
     title: ttReview.title,
     text: ttReview.text,
     rating: ttReview.rating,
     user: ttReview.user.nickName,
     created: ttReview.dateCreated,
-    dimensions: ttReview.dimensions.map((dimension: any) => {
+    dimensions: ttReview.dimensions.map(<T extends DimensionReview>(dimension: T) => {
       return {
         id: dimension.dimensionId,
         label: dimension.dimensionLabel,
@@ -21,7 +61,7 @@ function review(ttReview: any): ReviewTypes.Review {
   };
 }
 
-function reviewSummary(ttSummary: any): ReviewTypes.ReviewSummary {
+function reviewSummary<T extends TTSummary>(ttSummary: T): ReviewTypes.ReviewSummary {
   return {
     id: ttSummary.sku,
     averageRating: ttSummary.averageRating,
@@ -29,7 +69,7 @@ function reviewSummary(ttSummary: any): ReviewTypes.ReviewSummary {
   };
 }
 
-function reviewStatistics(ttSummary: any): ReviewTypes.ReviewStatistics {
+function reviewStatistics<T extends TTSummary>(ttSummary: T): ReviewTypes.ReviewStatistics {
   return {
     id: ttSummary.sku,
     averageRating: ttSummary.averageRating,
@@ -41,8 +81,8 @@ function reviewStatistics(ttSummary: any): ReviewTypes.ReviewStatistics {
       };
     }),
     dimensionAverages: ttSummary.dimensions
-      .filter((dimension: any) => dimension.average !== null)
-      .map((dimension: any) => {
+      .filter(<T extends DimensionSummary>(dimension: T) => dimension.average !== null)
+      .map(<T extends DimensionSummary>(dimension: T) => {
         return {
           id: dimension.id,
           label: dimension.label,
