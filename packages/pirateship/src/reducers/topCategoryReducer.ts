64a5f5467311ef7c9f1cbe5bb348f1559b978b80
alexsavosh <olexandor.s@gmail.com>
2020-05-18 10:57:18 +0300

    feat(any-occurrences): Replace all occurrences of "any" in /pirateship/src/reducers

--- packages/pirateship/src/reducers/topCategoryReducer.ts
+++ packages/pirateship/src/reducers/topCategoryReducer.ts
@@ -1,16 +1,20 @@
-import {
-  UPDATE_TOP_CATEGORIES
-} from '../lib/constants';
+import { UPDATE_TOP_CATEGORIES } from '../lib/constants';
+import { CommerceTypes } from '@brandingbrand/fscommerce';
 
 const initialState = { categories: [] };
 
 export interface TopCategoryStore {
-  categories: any[];
+  categories: CommerceTypes.Category[];
+}
+
+export interface TopCategoryAction {
+  type: 'UPDATE_TOP_CATEGORIES';
+  data: CommerceTypes.Category[];
 }
 
 export default function topCategoryReducer(
   topCategoryStore: TopCategoryStore = initialState,
-  action: any
+  action: TopCategoryAction
 ): TopCategoryStore {
   if (action.type === UPDATE_TOP_CATEGORIES) {
     return {
