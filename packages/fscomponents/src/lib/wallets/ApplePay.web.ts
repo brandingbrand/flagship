5c3d41b382d98de542d97a62055682fd239207b0
alexsavosh <olexandor.s@gmail.com>
2020-05-19 11:32:32 +0300

    refactor(fscomponents): replace all occurrences of "any" in ApplePay
    brandingbrand-source-id: 3efb2cfe21635a2b68e54cfea58fb8ed1345b1b3

--- packages/fscomponents/src/lib/wallets/ApplePay.web.ts
+++ packages/fscomponents/src/lib/wallets/ApplePay.web.ts
@@ -1,29 +1,42 @@
 import { default as ApplePayBase } from './ApplePay.base';
 
+export interface ApplePaySession {
+  openPaymentSetup?: (identifier: string) => void;
+  canMakePaymentsWithActiveCard?: (identifier: string) => void;
+}
+
+export type WindowApplePay = Window & {
+  ApplePaySession?: ApplePaySession;
+};
+
 export default class ApplePayWeb extends ApplePayBase {
-  theWindow: any;
+  theWindow: WindowApplePay;
 
   constructor(merchantIdentifier: string) {
     super(merchantIdentifier);
 
-    this.theWindow = window;
+    this.theWindow = window as WindowApplePay;
   }
 
   isEnabled(): boolean {
-    return this.theWindow && this.theWindow.ApplePaySession;
+    return this.theWindow && !!this.theWindow.ApplePaySession;
   }
 
   canCallSetup(): boolean {
     return this.theWindow
-      && this.theWindow.ApplePaySession
-      && this.theWindow.ApplePaySession.openPaymentSetup;
+      && !!this.theWindow.ApplePaySession
+      && !!this.theWindow.ApplePaySession.openPaymentSetup;
   }
 
   async hasActiveCard(): Promise<boolean> {
     if (this.theWindow && this.theWindow.ApplePaySession) {
       const ApplePaySession = this.theWindow.ApplePaySession;
 
-      return ApplePaySession.canMakePaymentsWithActiveCard(this.merchantIdentifier);
+      if (!ApplePaySession.canMakePaymentsWithActiveCard) {
+        return false;
+      }
+      ApplePaySession.canMakePaymentsWithActiveCard(this.merchantIdentifier);
+      return true;
     } else {
       return Promise.reject(new Error('Apple Pay is not enabled on this device.'));
     }
@@ -33,7 +46,12 @@ export default class ApplePayWeb extends ApplePayBase {
     if (this.theWindow && this.theWindow.ApplePaySession) {
       const ApplePaySession = this.theWindow.ApplePaySession;
 
-      return ApplePaySession.openPaymentSetup(this.merchantIdentifier);
+      if (!ApplePaySession.openPaymentSetup) {
+        return false;
+      }
+
+      ApplePaySession.openPaymentSetup(this.merchantIdentifier);
+      return true;
     } else {
       return Promise.reject(new Error('Apple Pay is not enabled on this device.'));
     }
