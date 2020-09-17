b7a5b619999af591838a7dcdb26fcddc2cf056da
Brett Weissbart <brett@brandingbrand.com>
2020-09-17 13:31:07 -0400

    feat(flagship): implement react-native-permissions v2 support
    Usage of the library is described in their repo:
    https://github.com/react-native-community/react-native-permissions#methods
    
    BREAKING CHANGE: This upgrades react-native-permissions from v1 to v2 and adds config support for
    adding the appropriate pods and permissions to the ios and android projects. Permissions v2 has a
    new API that will require changes for existing implementations.
    
    brandingbrand-source-id: f316fc682300fac97218f5a0e617ac60c1aec74a

--- packages/flagship/src/types.ts
+++ packages/flagship/src/types.ts
@@ -9,6 +9,62 @@ export enum TargetedDevices {
   Universal = 'Universal'
 }
 
+export type IOSPermissionKeys =
+  'APP_TRACKING_TRANSPARENCY'
+  | 'BLUETOOTH_PERIPHERAL'
+  | 'CALENDARS'
+  | 'CAMERA'
+  | 'CONTACTS'
+  | 'FACE_ID'
+  | 'LOCATION_ALWAYS'
+  | 'LOCATION_WHEN_IN_USE'
+  | 'MEDIA_LIBRARY'
+  | 'MICROPHONE'
+  | 'MOTION'
+  | 'NOTIFICATIONS'
+  | 'PHOTO_LIBRARY'
+  | 'REMINDERS'
+  | 'SIRI'
+  | 'SPEECH_RECOGNITION'
+  | 'STOREKIT';
+
+export type AndroidPermissionKeys =
+ 'ACCEPT_HANDOVER'
+ | 'ACCESS_BACKGROUND_LOCATION'
+ | 'ACCESS_COARSE_LOCATION'
+ | 'ACCESS_FINE_LOCATION'
+ | 'ACTIVITY_RECOGNITION'
+ | 'ADD_VOICEMAIL'
+ | 'ANSWER_PHONE_CALLS'
+ | 'BODY_SENSORS'
+ | 'CALL_PHONE'
+ | 'CAMERA'
+ | 'GET_ACCOUNTS'
+ | 'PROCESS_OUTGOING_CALLS'
+ | 'READ_CALENDAR'
+ | 'READ_CALL_LOG'
+ | 'READ_CONTACTS'
+ | 'READ_EXTERNAL_STORAGE'
+ | 'READ_PHONE_NUMBERS'
+ | 'READ_PHONE_STATE'
+ | 'READ_SMS'
+ | 'RECEIVE_MMS'
+ | 'RECEIVE_SMS'
+ | 'RECEIVE_WAP_PUSH'
+ | 'RECORD_AUDIO'
+ | 'SEND_SMS'
+ | 'USE_SIP'
+ | 'WRITE_CALENDAR'
+ | 'WRITE_CALL_LOG'
+ | 'WRITE_CONTACTS'
+ | 'WRITE_EXTERNAL_STORAGE';
+
+export interface UsageDescriptionIOS {
+  key: string;
+  string?: string;
+  array?: string[];
+}
+
 export interface Config {
   name: string;
   displayName: string;
@@ -76,11 +132,12 @@ export interface Config {
 
   enabledCapabilitiesIOS?: string[];
   entitlementsFileIOS?: string;
-  usageDescriptionIOS?: {
-    key: string;
-    string?: string;
-    array?: string[];
-  }[];
+  usageDescriptionIOS?: UsageDescriptionIOS[];
+
+  permissions?: {
+    ios?: {[k in IOSPermissionKeys]: string};
+    android?: AndroidPermissionKeys[];
+  };
 
   UIBackgroundModes?: {
     string: string;
