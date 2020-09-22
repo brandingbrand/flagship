65d26f2514b790cfa41d6b283503d71ea37cdbfc
Nathan Sankbeil <nathan.sankbeil@brandingbrand.com>
2020-09-22 13:13:27 -0400

    fix(fscomponents): fix imports of tcomb-form-native
    tcomb-form-native modifies the exported object after exporting, which causes issues with
    transpilation.
    
    brandingbrand-source-id: 20b1fea42ab22f24a67716233de205d8771206c0

--- packages/fscomponents/src/components/AddressForm.tsx
+++ packages/fscomponents/src/components/AddressForm.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-// @ts-ignore TODO: Update tcomb-form-native to support typing
-import * as t from 'tcomb-form-native';
+// Using import with tcomb-form-native seems to cause issues with the object being undefined.
+const t = require('tcomb-form-native');
 import { cloneDeep, merge, pickBy } from 'lodash-es';
 import { emailRegex } from '../lib/email';
 import { Form, FormLabelPosition } from './Form';
