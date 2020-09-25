ef08374596dff108a538b1adfcba5f08c57c6d4b
Alexandr <alexandrkorsia@gmail.com>
2020-09-25 14:40:48 +0300

    feat(fsproductdetail): add PDPGhost component
    brandingbrand-source-id: 1814a7668ba727df3babb1154cd2ef432dc0af18

--- packages/fsi18n/src/types.ts
+++ packages/fsi18n/src/types.ts
@@ -336,6 +336,7 @@ export interface ProductIndexTranslations<KeyType> {
   noResults: KeyType;
   resetFilters: KeyType;
   loadMore: KeyType;
+  addToBag: KeyType;
 }
 
 export interface CheckoutTranslations<KeyType> {
