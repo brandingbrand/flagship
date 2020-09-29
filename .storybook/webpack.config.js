aff92cab32b453c2748354706204e8da49172740
Cauldrath <ben@brandingbrand.com>
2020-09-29 16:19:23 -0400

    fix(fsweb): update to newer version of async-storage
    Web support was added in 1.9.0. This is updating to 1.12.0 in the yarn.lock, though.
    
    brandingbrand-source-id: 89c1b03b226f47a1a5f7b722950594aad3aadfa3

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -44,7 +44,13 @@ module.exports = ({ config, env }) => {
   ];
 
   config.resolve.alias["react-native"] = "react-native-web";
-  config.resolve.alias["@react-native-community/async-storage"] = "@react-native-community/async-storage/packages/storage-web/build/index.js";
+  config.resolve.alias["react-native-svg"] = "svg";
+  config.resolve.alias['react-native-web/dist/exports/DatePickerIOS'] = '@react-native-community/datetimepicker';
+  config.resolve.alias['react-native-web/dist/exports/PickerIOS'] = 'react-native-web/dist/exports/Picker';
+  config.resolve.alias['react-native-web/dist/exports/ProgressBarAndroid'] = 'react-native-web/dist/exports/ProgressBar';
+  config.resolve.alias['react-native-web/dist/exports/ProgressViewIOS'] = 'react-native-web/dist/modules/UnimplementedView';
+  config.resolve.alias['react-native-web/dist/exports/SegmentedControlIOS'] = 'react-native-web/dist/modules/UnimplementedView';
+  config.resolve.alias['react-native-web/dist/exports/WebView'] = 'react-native-web-webview';
 
   // Disable __DEV__ mode (this is traditionally set by react-native)
   config.plugins.push(new webpack.DefinePlugin({
