7511d604ad3f2650f46371836b365d30956a5eeb
Cauldrath <ben@brandingbrand.com>
2020-10-05 14:46:55 -0400

    fix(fsapp): update typing for pirateship lib
    brandingbrand-source-id: a3358a12e65012084a82f9e2d190e13ad01b4b28

--- packages/fscommerce/src/Commerce/types/Image.ts
+++ packages/fscommerce/src/Commerce/types/Image.ts
@@ -4,7 +4,6 @@ import { ImageURISource } from 'react-native';
  * Information about the location and dimensions of a remote image.
  */
 export interface Image extends ImageURISource {
-  src: string;
   /**
    * Text to provide additional context about an image or to be displayed if the image
    * cannot be loaded.
