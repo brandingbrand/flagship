49998d30f143e2ededd011506720b2c2c09336fc
Nathan Sankbeil <nathan.sankbeil@brandingbrand.com>
2020-10-27 15:08:42 -0400

    fix: fix android release variant compilation
    - adds path function to get debug android src folder
    - adds init step to rename debug src folder structure to match package
    - fixes flipper package name
    - moves flipper to only be included in debug

--- packages/flagship/src/commands/init.ts
+++ packages/flagship/src/commands/init.ts
@@ -142,6 +142,7 @@ function initAndroid(
   rename.source('FLAGSHIP', configuration.name, 'android');
   rename.source('CONFIG_BUNDLE_ID', pkgId, 'android');
   rename.pkgDirectory(TEMPLATE_ANDROID_PACKAGE, pkgId, path.android.mainPath(), 'java');
+  rename.pkgDirectory(TEMPLATE_ANDROID_PACKAGE, pkgId, path.android.debugPath(), 'java');
 
   fastlane.configure(path.android.fastfilePath(), configuration); // Update Fastfile
 
