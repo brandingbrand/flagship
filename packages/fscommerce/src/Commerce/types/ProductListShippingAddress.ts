2d072485bcf8d3ff0d761d02fd1a158798e74460
buticm <mikhail@brandingbrand.com>
2020-11-17 12:11:03 -0500

    feat(fscommerce): customer's product lists in fssalesforce package
    Added the interfaces we will implement in fssalesforce package
    to support customer's product lists.
    
    brandingbrand-source-id: 35ca43c500b3dc9d1223cd703b6cdbfe660061c2

---
+++ packages/fscommerce/src/Commerce/types/ProductListShippingAddress.ts
@@ -0,0 +1,24 @@
+/**
+ * Document representing a product list shipping address.
+ */
+export interface ProductListShippingAddress {
+  /**
+   * The id of this address.
+   */
+  id: string;
+
+  /**
+   * The city of this address.
+   */
+  city: string;
+
+  /**
+   * The first name of this address.
+   */
+  firstName: string;
+
+  /**
+   * The last name of this address.
+   */
+  lastName: string;
+}
