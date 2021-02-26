ec4a80f1259a8beb27febaf566a469c0fb69e0e5
buticm <mikhail@brandingbrand.com>
2021-02-26 18:16:56 -0500

    fix(fscommerce): session manager
    BREAKING CHANGE:
    - allow only one invocation of the session manager `get` method at a time.
    - provide types for the `fetchCart` method to allow only one invocation of the method at a time.

--- packages/fscommerce/src/Commerce/sessions/CommerceCookieSessionManager.web.ts
+++ packages/fscommerce/src/Commerce/sessions/CommerceCookieSessionManager.web.ts
@@ -1,13 +1,19 @@
-import CommerceSessionManager from './CommerceSessionManager';
+import CommerceSessionManager, { CommerceSessionManagerOptions } from './CommerceSessionManager';
 import { SessionToken } from '../types/SessionToken';
+
+export interface CommerceCookieSessionManagerOptions extends CommerceSessionManagerOptions {
+  sessionCookiesToToken: () => Promise<SessionToken | null>;
+  restoreCookies?: () => Promise<void>;
+}
 
 /**
  * Implementation of CommerceSessionManager token based authentication(jwt, oauth, etc.)
  */
 export default class CommerceCookieSessionManager extends CommerceSessionManager {
-  options: any;
+  options: CommerceCookieSessionManagerOptions;
+  getPromise!: Promise<SessionToken | null>;
 
-  constructor(options: any) {
+  constructor(options: CommerceCookieSessionManagerOptions) {
     super(options);
     this.options = options;
   }
@@ -16,40 +22,108 @@ export default class CommerceCookieSessionManager extends CommerceSessionManager
     return true;
   }
 
-  async get(): Promise<SessionToken> {
-    let token: SessionToken | null = null;
+  /**
+   * Returns current token for the session.
+   *
+   * Only one invocation at a time is allowed. If it's called again
+   * it will have to wait until the previous invocation is complete.
+   *
+   * The token is cached locally and re-used until it expires.
+   *
+   * The token is refreshed automatically upon expiration if `refreshToken` method was provided.
+   *
+   * If no token exist it will attempt to convert current session into JWT,
+   * but only if the cookie `dwanonymous_*` is present.
+   * Otherwise, it will create new token for the guest user.
+   *
+   * @returns {Promise.<SessionToken | null>} A Promise representing token
+   */
+  // tslint:disable:cyclomatic-complexity
+  async get(): Promise<SessionToken | null> {
+    // allow no more than one invocation at a time
+    this.getPromise = (this.getPromise || Promise.resolve()).then(async () => {
+      let token: SessionToken | null | undefined = this.token;
 
-    try {
-      token = await this.options.sessionCookiesToToken();
-    } catch (e) {
-      /* let it fail sliently */
-    }
+      // we have expired token stored locally
+      if (token && this.isExpired(token)) {
+        // refresh it
+        token = await this.options.refreshToken(token);
 
-    if (token) {
-      return token;
-    }
-
-    if (this.options.restoreCookies) {
-      try {
-        await this.options.restoreCookies();
-        token = await this.options.sessionCookiesToToken();
-      } catch (e) {
-        /* let it fail sliently */
+        // got new token - store it
+        if (token) {
+          await this.set(token);
+        }
       }
 
+      // got old token
       if (token) {
         return token;
       }
-    }
 
-    return this.options.createGuestToken();
+      // no token - get a token from the session
+      const sessionCookiesPresent = document.cookie?.includes('dwanonymous_');
+
+      if (sessionCookiesPresent) {
+        try {
+          token = await this.options.sessionCookiesToToken();
+        } catch (e) {
+          /* let it fail sliently */
+        }
+      }
+
+      // got a token - store it
+      if (token) {
+        await this.set(token);
+
+        return token;
+      }
+
+      // no token - try to restore the cookie and the a token from the session
+      if (this.options.restoreCookies) {
+        try {
+          await this.options.restoreCookies();
+
+          const sessionCookiesPresent = document.cookie?.includes('dwanonymous_');
+
+          if (sessionCookiesPresent) {
+            token = await this.options.sessionCookiesToToken();
+          }
+        } catch (e) {
+          /* let it fail sliently */
+        }
+      }
+
+      // got a token - store it
+      if (token) {
+        await this.set(token);
+
+        return token;
+      }
+
+      // no token - try to create new guest token
+      token = await this.options.createGuestToken();
+
+      // got a token - store it
+      if (token) {
+        await this.set(token);
+
+        return token;
+      }
+
+      // no token
+      return null;
+    });
+
+    return this.getPromise;
   }
 
   async set(token: SessionToken): Promise<boolean> {
+    this.token = token;
+
     return true;
   }
 
-  async restore(): Promise<SessionToken> {
+  async restore(): Promise<SessionToken | null> {
     return this.get();
   }
 }
