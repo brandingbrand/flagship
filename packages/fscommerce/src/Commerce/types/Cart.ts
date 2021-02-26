35b9874425f250315637f53f66a80b1b90a3c097
buticm <mikhail@brandingbrand.com>
2021-02-26 18:16:56 -0500

    fix(fscommerce): session manager
    BREAKING CHANGE:
    - allow only one invocation of the session manager `get` method at a time.
    - provide types for the `fetchCart` method to allow only one invocation of the method at a time.
    
    brandingbrand-source-id: 095e8d1ec4a1803b7cae6485837845ec5da437d1

--- packages/fscommerce/src/Commerce/types/Cart.ts
+++ packages/fscommerce/src/Commerce/types/Cart.ts
@@ -38,6 +38,12 @@ export interface Cart<T extends CartItem = CartItem> {
   items: T[];
 
   /**
+   * An array of products with detailed info for variants in the cart, their master
+   * products and bonus products in the cart.
+   */
+  product_details?: Product[];
+
+  /**
    * The total cost of the products in the cart before shipping feeds and taxes have
    * been added.
    *
