635980e4694bcf2b6bc13cf7fd5c760387a772eb
Andy Schwob <andy.schwob@brandingbrand.com>
2021-03-22 14:55:28 -0400

    feat(fsapp): allow custom nav bar styles on makeModal
    brandingbrand-source-id: 0ad74b4cdd428befcad38bf9cdc8e19effe7b3ce

--- packages/fsapp/src/beta-app/modal/types.ts
+++ packages/fsapp/src/beta-app/modal/types.ts
@@ -1,6 +1,7 @@
 import type { ComponentType } from 'react';
 import type { ModalProps, StyleProp, ViewStyle } from 'react-native';
 import type { ModalOptions as RNModalOptions } from 'react-native-navigation';
+import type { TopBarStyle } from '../router/types';
 
 export interface ModalService {
   showModal<T>(modal: ModalComponentType<T, void>): Promise<T>;
@@ -24,4 +25,5 @@ export type ModalComponentType<T = void, P = {}> = ComponentType<ModalComponentP
   readonly __resolves: T;
   readonly definitionId: string;
   readonly options?: ModalOptions;
+  readonly topBarOptions?: TopBarStyle;
 };
