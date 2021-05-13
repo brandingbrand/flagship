ec8a6a484f6dff916d8f21ae4b761d6fe7a9d8ba
wSedlacek <wsedlacekc@gmail.com>
2021-05-13 09:40:06 -0700

    feat(fscomponents): serializable v1
    brandingbrand-source-id: 2017f53da89e88b60c7ea3b4955f7b797b87ee19

---
+++ packages/fscomponents/src/components/serializable/v1/index.ts
@@ -0,0 +1,7 @@
+export * from './SerializableText';
+export * from './SerializableTooltip';
+export * from './SerializableView';
+export * from './SerializableButton';
+export * from './SerializableImage';
+export * from './SerializableVideoBlock';
+export * from './SerializableVideoBlock.types';
