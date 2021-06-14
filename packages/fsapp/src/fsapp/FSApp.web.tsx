035646ee84c272bbbd9db92efe2bd3d07ed05e38
Nathan Sankbeil <nathan.sankbeil@brandingbrand.com>
2021-06-14 11:45:56 -0400

    feat(fsapp): expose hydrate RNW config option
    brandingbrand-source-id: 3eaa0d28b94b1e483217e2421939ce4ca3d1cbd7

--- packages/fsapp/src/fsapp/FSApp.web.tsx
+++ packages/fsapp/src/fsapp/FSApp.web.tsx
@@ -49,6 +49,7 @@ export class FSApp extends FSAppBase {
       }
       AppRegistry.runApplication('Flagship', {
         rootTag,
+        hydrate: this.appConfig.hydrate,
         initialProps: {
           appConfig: this.appConfig,
           api: this.api,
