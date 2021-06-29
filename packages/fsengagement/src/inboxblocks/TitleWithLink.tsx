ae039f55a6b85f44adc88e52192a0810a9e1675d
Andy Schwob <andy.schwob@brandingbrand.com>
2021-06-29 16:11:51 -0400

    feat(fsengagement): switching to new component library
    brandingbrand-source-id: ea32f2e88d01f9a6e01a547f8ccee53bd81271d0

---
+++ packages/fsengagement/src/inboxblocks/TitleWithLink.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  StyleProp,
+  StyleSheet,
+  TextStyle,
+  TouchableOpacity,
+  View,
+  ViewStyle
+} from 'react-native';
+import { EngagementContext } from '../lib/contexts';
+import { TextBlock } from './TextBlock';
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flex: 3,
+    alignItems: 'flex-start'
+  },
+  flexContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  linkContainer: {
+    flex: 1,
+    alignItems: 'flex-end'
+  }
+});
+
+export interface TitleWithLinkProps {
+  contents: any;
+  containerStyle?: StyleProp<ViewStyle>;
+  titleContainer?: StyleProp<TextStyle>;
+}
+
+export const TitleWithLink: React.FC<TitleWithLinkProps> = React.memo(props => {
+  const { containerStyle, contents, titleContainer } = props;
+
+  const onPress = (link: string) => () => {
+    const { handleAction } = React.useContext(EngagementContext);
+    handleAction({
+      type: 'deep-link',
+      value: link
+    });
+  };
+
+  return (
+    <View style={containerStyle}>
+      <View style={styles.flexContainer}>
+        <View style={[styles.titleContainer, titleContainer]}>
+          <TextBlock {...contents.Text} />
+        </View>
+        {!!contents?.TextLink?.enabled && (
+          <View style={styles.linkContainer}>
+            <TouchableOpacity
+              activeOpacity={1}
+              onPress={onPress(contents.TextLink.link)}
+            >
+              <TextBlock {...contents.TextLink} />
+            </TouchableOpacity>
+
+          </View>
+        )}
+      </View>
+    </View>
+  );
+});
