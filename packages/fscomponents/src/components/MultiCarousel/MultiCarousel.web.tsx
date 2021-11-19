74c583247c792d39fe3b695d7a53f3548de12de2
Matt Clark <matt.clark@brandingbrand.com>
2021-11-19 06:09:59 -0800

    fix(fscomponents): snaps carousel back to first image when autoscrolling and reaches the end
    brandingbrand-source-id: 63e564bb146c0b89ffe53430b1ac9a65887246af

--- packages/fscomponents/src/components/MultiCarousel/MultiCarousel.web.tsx
+++ packages/fscomponents/src/components/MultiCarousel/MultiCarousel.web.tsx
@@ -281,9 +281,13 @@ export const MultiCarousel = <ItemT, >(props: MultiCarouselProps<ItemT>) => {
   );
 
   const goToNext = useCallback(async (options?: GoToOptions | GestureResponderEvent) => {
-    const nextIndex = currentIndex + 1 > numberOfPages - 1 ? numberOfPages - 1 : currentIndex + 1;
+    const nextIndex = currentIndex + 1 > numberOfPages - 1 ? 0 : currentIndex + 1;
+    const animationOptions =
+      nextIndex !== 0 &&
+      options &&
+      'animated' in options ? options : {animated: false};
 
-    await goTo(nextIndex, options && 'animated' in options ? options : undefined);
+    await goTo(nextIndex, animationOptions);
   }, [goTo, currentIndex, numberOfPages]);
 
   const goToPrev = useCallback(async (options?: GoToOptions | GestureResponderEvent) => {
@@ -309,7 +313,6 @@ export const MultiCarousel = <ItemT, >(props: MultiCarouselProps<ItemT>) => {
   useEffect(() => {
     if (autoplay && numberOfPages > 0 && !animating && shouldPlay) {
       autoplayTimeout.current = setTimeout(async () => {
-        const nextIndex = currentIndex + 1;
         const options = {
           animated: true
         };
@@ -317,12 +320,7 @@ export const MultiCarousel = <ItemT, >(props: MultiCarouselProps<ItemT>) => {
         if (autoplayTimeoutDuration <= 300) {
           options.animated = false;
         }
-
-        if (nextIndex < numberOfPages) {
-          await goToNext(options);
-        } else {
-          await goToPrev(options);
-        }
+        await goToNext(options);
       }, autoplayTimeoutDuration);
       return () => {
         if (autoplayTimeout.current) {
