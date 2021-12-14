ae7e5e690742ecc1af9ece44ede0e5e7af758edf
wSedlacek <wsedlacekc@gmail.com>
2021-12-14 12:29:30 -0800

    refactor(fslinker): migrate to nx library
    brandingbrand-source-id: 0967b10a1bb8ae0af40f8fc2ca1ee5d7a7503462

---
+++ libs/fslinker/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'fslinker',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/fslinker',
+};
