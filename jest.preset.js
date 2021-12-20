39406eed2999f0b1e754874e69a39d4f46cff07b
wSedlacek <wsedlacekc@gmail.com>
2021-12-20 12:06:13 -0800

    chore(repo): project configuration
    brandingbrand-source-id: 3ab3bafa63dedc191e4c5d3329b6d237a004515b

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,9 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  haste: {
+    defaultPlatform: 'native',
+    platforms: ['web', 'native', 'ios', 'android'],
+  },
+};
