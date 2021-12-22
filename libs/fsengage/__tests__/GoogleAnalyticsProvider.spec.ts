8ab9033a6f9e4c708ce48401ba16f5f5ad00a393
wSedlacek <wsedlacekc@gmail.com>
2021-12-21 16:26:23 -0800

    chore(repo): bump sinon from 8.1.1 to 12.0.1
    Bumps [sinon](https://github.com/sinonjs/sinon) from 8.1.1 to 12.0.1.
    - [Release notes](https://github.com/sinonjs/sinon/releases)
    - [Changelog](https://github.com/sinonjs/sinon/blob/master/docs/changelog.md)
    - [Commits](https://github.com/sinonjs/sinon/compare/v8.1.1...v12.0.1)
    
     ---
    updated-dependencies:
    - dependency-name: sinon
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    brandingbrand-source-id: 3b00b2a6da71d3f140b2d3154b400afe858b2fac

--- libs/fsengage/__tests__/GoogleAnalyticsProvider.spec.ts
+++ libs/fsengage/__tests__/GoogleAnalyticsProvider.spec.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/unbound-method */
-// @ts-ignore using ts-sinon instead of sinon causes a conflict between @types/node and @types/react
 import { assert, stub } from 'sinon';
 import { Component } from 'react';
 
