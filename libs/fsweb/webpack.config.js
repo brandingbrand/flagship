d2e8d419d8c05290b3d752db95e30e3eb7721ac9
wSedlacek <william.sedlacek@icloud.com>
2022-01-12 20:52:14 -0800

    fix(fsweb): babel configuration
    brandingbrand-source-id: 2afcbd4a60c4a36fba3a3740d9537c9df0767c51

--- libs/fsweb/webpack.config.js
+++ libs/fsweb/webpack.config.js
@@ -94,10 +94,17 @@ const globalConfig = {
                 options: {
                   cacheDirectory: true,
                   presets: [
+                    'module:metro-react-native-babel-preset',
                     [
                       '@babel/preset-env',
                       {
+                        exclude: ['transform-typeof-symbol'],
                         modules: false,
+                        targets: {
+                          browsers: ['last 2 versions', 'ie >= 11'],
+                        },
+                        useBuiltIns: 'usage',
+                        corejs: 3,
                       },
                     ],
                   ],
@@ -149,10 +156,17 @@ const globalConfig = {
                 options: {
                   cacheDirectory: true,
                   presets: [
+                    'module:metro-react-native-babel-preset',
                     [
                       '@babel/preset-env',
                       {
+                        exclude: ['transform-typeof-symbol'],
                         modules: false,
+                        targets: {
+                          browsers: ['last 2 versions', 'ie >= 11'],
+                        },
+                        useBuiltIns: 'usage',
+                        corejs: 3,
                       },
                     ],
                   ],
