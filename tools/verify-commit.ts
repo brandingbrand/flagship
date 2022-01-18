9bd6de8f755cde4534e1cb0964c16da23bf49572
wSedlacek <william.sedlacek@icloud.com>
2022-01-17 23:27:19 -0800

    chore(workspace): extensive project scope verification
    brandingbrand-source-id: 81ce3ad3c43d4123c3a252657d768adf931cd83c

---
+++ tools/verify-commit.ts
@@ -0,0 +1,162 @@
+#!/usr/bin/env ts-node
+
+import parseCommit from '@commitlint/parse';
+import { Commit } from '@commitlint/types';
+
+import { logger } from '@nrwl/devkit';
+import { FsTree } from '@nrwl/tao/src/shared/tree';
+import { Workspace, Workspaces } from '@nrwl/tao/src/shared/workspace';
+
+import dedent from 'ts-dedent';
+import { prompt } from 'inquirer';
+import { exec } from 'child_process';
+import { readFile } from 'fs/promises';
+import { sep } from 'path';
+
+const commitMessageFile = process.argv[2] as string | undefined;
+const whitelistedFiles = ['tsconfig.base.json', 'workspace.json'];
+
+const isWithin = (file: string, path: string) => {
+  const fileDirs = file.split(sep);
+  return path.split(sep).every((dir, i) => fileDirs[i] === dir);
+};
+
+const getWorkspace = (): Workspace => {
+  return new Workspaces(process.cwd()).readWorkspaceConfiguration();
+};
+
+const getStagedFiles = async () => {
+  return new Promise<string[]>((resolve, reject) => {
+    exec('git diff --name-only --cached', {}, (error, stdout) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(stdout.trim().split('\n'));
+    });
+  });
+};
+
+const getChangedProjects = async (
+  workspace: Workspace,
+  stagedFiles: string[]
+): Promise<string[]> => {
+  return Object.entries(workspace.projects)
+    .filter(([, { root }]) => root !== '.' && stagedFiles.some((file) => isWithin(file, root)))
+    .map(([name]) => name);
+};
+
+const verifyBreakingChange = async (commit: Commit) => {
+  if (commit.notes.some(({ title }) => title === 'BREAKING CHANGE')) {
+    const { confirmed } = await prompt({
+      name: 'confirmed',
+      message: dedent`Your commit includes a breaking change, it will only be able to be
+                      released at the end of Q1 or Q3.
+
+                      Are you sure you want to continue?`,
+      type: 'confirm',
+    });
+
+    if (!confirmed) {
+      process.exit(1);
+    }
+  }
+};
+
+const verifyProjectScope = async (commit: Commit, changedProjects: string[]) => {
+  if (
+    commit.scope &&
+    changedProjects.length > 0 &&
+    changedProjects.some((project) => project !== commit.scope)
+  ) {
+    const changedProjectsString = changedProjects.join(', ');
+    const { confirmed } = await prompt({
+      name: 'confirmed',
+      message: dedent`Your change affects ${changedProjectsString} but your scope is ${commit.scope}.
+                      Typically you would want one commit for each project changed.
+
+                      Are you sure you want to continue?`,
+      type: 'confirm',
+    });
+
+    if (!confirmed) {
+      process.exit(1);
+    }
+  }
+};
+
+const verifyWorkspaceScope = async (commit: Commit, changedProjects: string[]) => {
+  if (commit.scope !== 'workspace' && changedProjects.length === 0) {
+    const { confirmed } = await prompt({
+      name: 'confirmed',
+      message: dedent`No projects seem to have been affected, but your scope is ${commit.scope}.
+                      Typically you would want to use the workspace scope when making changes that
+                      do not apply to a specific project.
+
+                      Are you sure you want to continue?`,
+      type: 'confirm',
+    });
+
+    if (!confirmed) {
+      process.exit(1);
+    }
+  }
+};
+
+const verifyClosedProjectReferences = async (workspace: Workspace, stagedFiles: string[]) => {
+  const closedSourceProjects = Object.entries(workspace.projects).filter(
+    ([, { root, tags }]) => root !== '.' && !tags?.includes('open-source')
+  );
+
+  const openFiles = stagedFiles
+    .filter((file) => !closedSourceProjects.some(([, { root }]) => isWithin(file, root)))
+    .filter((file) => !whitelistedFiles.includes(file));
+
+  const tree = new FsTree(process.cwd(), false);
+  for (const file of openFiles) {
+    const contents = tree.read(file)?.toString('utf-8');
+
+    for (const [project] of closedSourceProjects) {
+      if (contents?.includes(project)) {
+        const { confirmed } = await prompt({
+          name: 'confirmed',
+          message: dedent`Found reference to closed source project ${project} in ${file}.
+                          Typically closed source projects should only be referenced within their
+                          own files.
+
+                          Are you sure you want to continue?`,
+          type: 'confirm',
+        });
+
+        if (!confirmed) {
+          process.exit(1);
+        }
+      }
+    }
+  }
+};
+
+const main = async (commitFile: string | undefined) => {
+  if (commitFile === undefined) {
+    logger.error('No commit message provided');
+    process.exit(1);
+  }
+
+  const commitMessage = (await readFile(commitFile)).toString('utf-8');
+  const commit = await parseCommit(commitMessage);
+
+  const workspace = getWorkspace();
+  const stagedFiles = await getStagedFiles();
+
+  const changedProjects = await getChangedProjects(workspace, stagedFiles);
+
+  await verifyBreakingChange(commit);
+  await verifyProjectScope(commit, changedProjects);
+  await verifyWorkspaceScope(commit, changedProjects);
+  await verifyClosedProjectReferences(workspace, stagedFiles);
+};
+
+void main(commitMessageFile).catch((error: unknown) => {
+  logger.error(error);
+  process.exit(1);
+});
