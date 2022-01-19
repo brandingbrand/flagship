897c2dfcd0cf46dea2ac672da1ddea38e4696b4b
wSedlacek <william.sedlacek@icloud.com>
2022-01-19 13:01:19 -0800

    fix(fsapp): register legacy env configs
    brandingbrand-source-id: 0997c26bb53dcfc8a4b9dd0550eebe58c1bcc5aa

--- libs/fsapp/src/beta-app/env.ts
+++ libs/fsapp/src/beta-app/env.ts
@@ -1,11 +1,20 @@
-import { getEnvironmentConfigs, setDefaultEnvironment } from '@brandingbrand/fsenv';
+import {
+  getEnvironmentConfigs,
+  registerEnvironmentConfig,
+  setDefaultEnvironment,
+} from '@brandingbrand/fsenv';
 import { EnvSwitcher } from './lib/env-switcher';
 
 import * as projectEnvs from '../project_env_index';
 
-setDefaultEnvironment(EnvSwitcher.envName);
+for (const [name, environment] of Object.entries(projectEnvs)) {
+  if (environment) {
+    registerEnvironmentConfig(name, environment);
+  }
+}
 
-export const envs = projectEnvs.unset === null ? getEnvironmentConfigs() : projectEnvs;
+setDefaultEnvironment(EnvSwitcher.envName);
+export const envs = getEnvironmentConfigs();
 if (Object.keys(envs).length === 0) {
   throw new Error('Failed to load any envs, did you forget to run init?');
 }
