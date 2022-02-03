c2b6d0704bd18f0be8f5fe5e3a42374f718e29a7
wSedlacek <william.sedlacek@icloud.com>
2022-02-03 13:21:03 -0800

    fix(fscommerce): update product group
    brandingbrand-source-id: f85775643e5bf89c7c289aa492dbd7c99aa7d2c6

--- libs/fscommerce/src/Commerce/types/ProductGroup.ts
+++ libs/fscommerce/src/Commerce/types/ProductGroup.ts
@@ -1,5 +1,5 @@
 import { Product } from './Product';
-import Pageable from './Pageable';
+import { Pageable } from './Pageable';
 
 /**
  * Similar to a ProductIndex but with the group metadata removed
