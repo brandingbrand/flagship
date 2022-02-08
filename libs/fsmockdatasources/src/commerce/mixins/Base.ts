a01ded78ad27d8dc957086e183ad00cf13e4ea7f
Stefan Neamtu <stefan.neamtu@brandingbrand.com>
2022-02-08 17:10:32 -0500

    feat(fsmockdatasources): flesh out fetchProducts with simple logic
    - make the method usable when using mock data so unit tests pass
    
    brandingbrand-source-id: a64fabf96297d30589d96d76663d0770382ffb01

--- libs/fsmockdatasources/src/commerce/mixins/Base.ts
+++ libs/fsmockdatasources/src/commerce/mixins/Base.ts
@@ -1,11 +1,19 @@
 import { CommerceTypes } from '@brandingbrand/fscommerce';
-import { DefaultCurrencyCode } from '../../helpers';
+import { DefaultCurrencyCode, Products } from '../../helpers';
 
 export class Base {
   public minRefinements: number = 0;
   public storeCurrencyCode: string = DefaultCurrencyCode;
 
   async fetchProducts(ids: string[]): Promise<CommerceTypes.Product[]> {
-    throw new Error('Not implemented yet');
+    const products: CommerceTypes.Product[] = [];
+
+    ids.forEach((id) => {
+      const product = Products.find((product) => product.id === id);
+      if (product) {
+        products.push(product);
+      }
+    });
+    return products;
   }
 }
