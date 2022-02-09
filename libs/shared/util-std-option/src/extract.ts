4dac96db4535e00df2e5899944dda49a056ae5d1
Derek Wiers <derek.wiers@brandingbrand.com>
2022-02-09 11:46:49 -0600

    refactor(shared-util-std-option): changed some types dependencies
    brandingbrand-source-id: d3ad16e71598958a7b0fbfdd93dd3efe963fcf13

--- libs/shared/util-std-option/src/extract.ts
+++ libs/shared/util-std-option/src/extract.ts
@@ -1,7 +1,8 @@
+import { Lazy } from '@brandingbrand/types-utility';
 import { isNone, Option } from './option';
 
 export const extract =
-  <Value, NoneValue>(onNone: () => NoneValue) =>
+  <Value, NoneValue>(onNone: Lazy<NoneValue>) =>
   (input: Option<Value>): Value | NoneValue => {
     if (isNone(input)) {
       return onNone();
