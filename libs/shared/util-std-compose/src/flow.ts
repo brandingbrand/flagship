85fd9e08990a989f0f68c3b74281ca974fed974b
Derek Wiers <derek.wiers@brandingbrand.com>
2022-02-09 00:42:08 -0600

    feat(shared-util-std-compose): initialize project
    brandingbrand-source-id: 5c52b3470ad32c47d3883c8a115c868237922ed6

---
+++ libs/shared/util-std-compose/src/flow.ts
@@ -0,0 +1,69 @@
+export function flow<A extends readonly unknown[], B>(ab: (...a: A) => B): (...a: A) => B;
+export function flow<A extends readonly unknown[], B, C>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C
+): (...a: A) => C;
+export function flow<A extends readonly unknown[], B, C, D>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D
+): (...a: A) => D;
+export function flow<A extends readonly unknown[], B, C, D, E>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E
+): (...a: A) => E;
+export function flow<A extends readonly unknown[], B, C, D, E, F>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F
+): (...a: A) => F;
+export function flow<A extends readonly unknown[], B, C, D, E, F, G>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F,
+  fg: (f: F) => G
+): (...a: A) => G;
+export function flow<A extends readonly unknown[], B, C, D, E, F, G, H>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F,
+  fg: (f: F) => G,
+  gh: (g: G) => H
+): (...a: A) => H;
+export function flow<A extends readonly unknown[], B, C, D, E, F, G, H, I>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F,
+  fg: (f: F) => G,
+  gh: (g: G) => H,
+  hi: (h: H) => I
+): (...a: A) => I;
+export function flow<A extends readonly unknown[], B, C, D, E, F, G, H, I, J>(
+  ab: (...a: A) => B,
+  bc: (b: B) => C,
+  cd: (c: C) => D,
+  de: (d: D) => E,
+  ef: (e: E) => F,
+  fg: (f: F) => G,
+  gh: (g: G) => H,
+  hi: (h: H) => I,
+  ij: (i: I) => J
+): (...a: A) => J;
+export function flow(
+  firstFunction: (...params: unknown[]) => unknown,
+  ...functions: ((value: unknown) => unknown)[]
+): (...params: unknown[]) => unknown {
+  return (...params) => {
+    return functions.reduce((val, fn) => fn(val), firstFunction(...params));
+  };
+}
