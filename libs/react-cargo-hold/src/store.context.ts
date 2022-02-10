9891c155a7b36c7364239d6fae159d0c9c48d846
wSedlacek <william.sedlacek@icloud.com>
2022-02-10 08:51:57 -0800

    build(react-cargo-hold): audit build settings
    brandingbrand-source-id: 4904797e9653d8d3bf7e1c302eab519d2a2c8c6b

--- libs/react-cargo-hold/src/store.context.ts
+++ libs/react-cargo-hold/src/store.context.ts
@@ -1,4 +1,10 @@
-import { IStore } from '@brandingbrand/cargo-hold';
+import type { Context } from 'react';
+import type { IStore } from '@brandingbrand/cargo-hold';
+import type { InjectionToken } from '@brandingbrand/fslinker';
+
 import { provideContext } from '@brandingbrand/react-linker';
 
-export const StoreContext = provideContext<IStore | undefined>('StoreContext', undefined);
+// Explicity declaring the return type is necessary to avoid submodule type references
+export const StoreContext: InjectionToken<Context<IStore | undefined>> = provideContext<
+  IStore | undefined
+>('StoreContext', undefined);
