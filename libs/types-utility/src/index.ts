844921d71f582bcaa878f6731129d82b21c81474
Derek Wiers <derek.wiers@brandingbrand.com>
2022-02-16 14:02:07 -0600

    feat(types-utility): add MaybePromise
    brandingbrand-source-id: d3053f345253efd6d41bdd0343a711073a0bff5b

--- libs/types-utility/src/index.ts
+++ libs/types-utility/src/index.ts
@@ -1,6 +1,7 @@
 export * from './Constructor';
 export * from './DeepPartial';
 export * from './Lazy';
+export * from './MaybePromise';
 export * from './Optional';
 export * from './Predicate';
 export * from './TypeGuard';
