ea83601e372a232c53eb2063afbfeb31366422ee
Derek Wiers <derek.wiers@brandingbrand.com>
2022-02-22 19:39:17 -0600

    chore(repo): project configuration
    brandingbrand-source-id: ad607e90eadebc80853e75cb0f6413e1ebc93214

--- libs/react-cargo-hold/src/global-store.context.ts
+++ libs/react-cargo-hold/src/global-store.context.ts
@@ -4,5 +4,6 @@ import type { Context } from 'react';
 import { provideContext } from '@brandingbrand/react-linker';
 
 // Explicity declaring the return type is necessary to avoid submodule type references
-export const GlobalStoreContext: InjectionToken<Context<IStore | undefined>> =
-  provideContext<IStore | undefined>('GlobalStoreContext', undefined);
+export const GlobalStoreContext: InjectionToken<Context<IStore | undefined>> = provideContext<
+  IStore | undefined
+>('GlobalStoreContext', undefined);
