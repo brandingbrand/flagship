edfd22185042c39add5a2140fa8e8d5d2288c08f
wSedlacek <william.sedlacek@icloud.com>
2022-03-11 15:56:43 -0800

    fix(fsengage): remove leanplum provider on uat
    brandingbrand-source-id: b168c5d8bb8169c57510a6091f9d4661c042b218

---
+++ libs/fsengage/src/modules/analytics/providers/index.web.ts
@@ -0,0 +1,5 @@
+export * from './AnalyticsProvider';
+export { default as AnalyticsProvider } from './AnalyticsProvider';
+
+export { default as GoogleAnalyticsProvider } from './google/GoogleAnalyticsProvider';
+export * from './adobe/AdobeAnalyticsProvider';
