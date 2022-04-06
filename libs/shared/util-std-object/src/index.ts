bc43dba5d408316a42a4871f73613c841836e3fc
wSedlacek <william.sedlacek@icloud.com>
2022-04-06 08:05:57 -0700

    feat(shared-util-std-object): merge deep
    brandingbrand-source-id: cf0b9571dadc2b7de178ded45a7493884fe6a782

--- libs/shared/util-std-object/src/index.ts
+++ libs/shared/util-std-object/src/index.ts
@@ -2,3 +2,4 @@ export { defaults } from './defaults.util';
 export { defaultsDeep } from './defaults-deep.util';
 export { cloneDeep } from './clone-deep.util';
 export { isPlainObject } from './is-plain-object.util';
+export { mergeDeep } from './merge-deep.util';
