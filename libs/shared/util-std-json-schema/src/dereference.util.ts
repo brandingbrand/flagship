0a8efc027966d42b8bb9ea6c5f2d96275df30791
wSedlacek <william.sedlacek@icloud.com>
2022-04-06 08:03:57 -0700

    feat(shared-util-std-json-schema): dereference
    brandingbrand-source-id: 75a73fcbf0d59a30bd5f2c8a04781d62071df47d

---
+++ libs/shared/util-std-json-schema/src/dereference.util.ts
@@ -0,0 +1,61 @@
+import type { JSONSchema7, JSONSchema7Definition } from 'json-schema';
+
+const refRegex = /^#\/definitions\/(.*)$/;
+
+// eslint-disable-next-line complexity
+export const dereference = <T extends JSONSchema7Definition = JSONSchema7>(
+  schema: T,
+  parentDefinitions?: JSONSchema7['definitions']
+): T => {
+  if (schema === undefined) {
+    return schema;
+  }
+
+  if (typeof schema === 'boolean') {
+    return schema;
+  }
+
+  const jsonSchema: JSONSchema7 = schema;
+  const { $ref, definitions, ...otherProperties } = jsonSchema;
+  const currentDefinitions = { ...definitions, ...parentDefinitions };
+
+  if ($ref) {
+    const refMatch = refRegex.exec($ref);
+    if (refMatch !== null) {
+      const ref = refMatch[1] as string;
+      const definition = currentDefinitions[ref];
+      if (definition) {
+        if (typeof definition === 'object') {
+          return dereference(
+            {
+              ...definition,
+              ...otherProperties,
+            },
+            definitions
+          ) as T;
+        }
+
+        return definition as T;
+      }
+    }
+  }
+
+  return {
+    ...otherProperties,
+    ...('properties' in jsonSchema
+      ? {
+          properties: Object.fromEntries(
+            Object.entries(jsonSchema.properties ?? {}).map(
+              ([key, childSchema]) => [key, dereference(childSchema, definitions)] as const
+            )
+          ),
+        }
+      : {}),
+    ...('anyOf' in jsonSchema
+      ? { anyOf: jsonSchema.anyOf?.map((childSchema) => dereference(childSchema, definitions)) }
+      : {}),
+    ...('allOf' in jsonSchema
+      ? { allOf: jsonSchema.allOf?.map((childSchema) => dereference(childSchema, definitions)) }
+      : {}),
+  } as JSONSchema7 as T;
+};
--- libs/shared/util-std-json-schema/src/index.ts
+++ libs/shared/util-std-json-schema/src/index.ts
@@ -1,3 +1,4 @@
 export * from './is-super-set';
 export * from './merge-allof';
 export * from './consume';
+export * from './dereference.util';
