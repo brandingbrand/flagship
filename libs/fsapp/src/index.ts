3d4a04c488a71d840f2e86c20fb1573ebfbd1154
wSedlacek <william.sedlacek@icloud.com>
2022-07-12 09:34:12 -0700

    fix(fsapp): remove namespace exports
    These were causing babel errors when running with React Native.
    Typically TypeScript would remove these, however the TypeScript module needs to be set to es2020 for dynamic imports so they have to be manually removed.
    
    brandingbrand-source-id: 98d739ca89379f48ea060d1855c10b6965e46180

--- libs/fsapp/src/index.ts
+++ libs/fsapp/src/index.ts
@@ -1,8 +1,9 @@
+import * as FSAppTypes from './types';
+
 export * from './beta-app';
-
 export { default as Navigator } from './lib/nav-wrapper';
-
 export * from './lib/helpers';
-
 export { FSApp } from './fsapp/FSApp';
-export * as FSAppTypes from './types';
+
+// eslint-disable-next-line unicorn/prefer-export-from -- Requires newer module versions not in RN
+export { FSAppTypes };
