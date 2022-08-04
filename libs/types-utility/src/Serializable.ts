1b9464404de9bfa6755aa2b29988aeb307dca7c8
wSedlacek <william.sedlacek@icloud.com>
2022-08-04 11:08:40 -0700

    refactor(types-utility): remove pascal case
    brandingbrand-source-id: 6c3ade67447925ebb1e495d3bacde3d794eec3c4

--- libs/types-utility/src/Serializable.ts
+++
@@ -1,5 +0,0 @@
-export type JsonPrimitive = boolean | number | string | null;
-export type JsonObject = { [Key in number | string]?: JsonValue | undefined };
-export type JsonArray = JsonValue[];
-export type JsonValue = JsonArray | JsonObject | JsonPrimitive;
-export type VerifyIsSerializable<T extends JsonValue> = T;
