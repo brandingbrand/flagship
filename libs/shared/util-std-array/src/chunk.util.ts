5579e8616555cc0e2553aac34ec69e964a2ccefe
wSedlacek <william.sedlacek@icloud.com>
2022-08-23 17:31:47 -0700

    fix(shared-util-std-array): chunking indexes
    brandingbrand-source-id: ad12f90336c5897e061bcc34052c2b6702e09fc3

--- libs/shared/util-std-array/src/chunk.util.ts
+++ libs/shared/util-std-array/src/chunk.util.ts
@@ -2,7 +2,8 @@ export const chunk =
   (chunkSize: number) =>
   <Element>(input: Element[]): Element[][] => {
     const resultLength = chunkSize ? Math.ceil(input.length / chunkSize) : 1;
-    return [...new Array(resultLength)].map((_element, index) =>
-      input.slice(index, index + chunkSize)
-    );
+    return [...new Array(resultLength)].map((_element, index) => {
+      const mappedIndex = index * chunkSize;
+      return input.slice(mappedIndex, mappedIndex + chunkSize);
+    });
   };
