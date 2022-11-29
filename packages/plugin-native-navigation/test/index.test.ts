c059e7545f0213c00ee3c96bb866469eb3aa7738
Christopher Herman <crherman7@gmail.com>
2022-11-29 10:24:31 -0500

    feat(fs12): FCR-3519 plugin unit tests (#2425)

--- packages/plugin-native-navigation/test/index.test.ts
+++ packages/plugin-native-navigation/test/index.test.ts
@@ -1,9 +1,74 @@
+import path from "path";
+import { path as pathk } from "@brandingbrand/kernel-core";
+
+import { ios, android } from "../src";
+
+global.process.cwd = () => path.resolve(__dirname, "..", "..", "..");
+
+jest.mock("glob", () => ({
+  sync: () => [""],
+}));
+
+jest.mock("@brandingbrand/kernel-core", () => {
+  const core = jest.requireActual("@brandingbrand/kernel-core");
+
+  return {
+    ...core,
+    fs: {
+      ...core.fs,
+      chmod: jest.fn().mockResolvedValue(undefined),
+    },
+  };
+});
+
+jest.mock("../src/utils/dependencies");
+
 describe("plugin-native-navigation", () => {
-  it("ios", () => {
-    expect("").toMatch("");
+  it("ios", async () => {
+    jest.mock(
+      pathk.project.resolve(
+        "node_modules",
+        "react-native-navigation",
+        "autolink",
+        "postlink",
+        "postLinkIOS.js"
+      )
+    );
+
+    const postLinkIOS = require(pathk.project.resolve(
+      "node_modules",
+      "react-native-navigation",
+      "autolink",
+      "postlink",
+      "postLinkIOS.js"
+    ));
+
+    await ios();
+
+    expect(postLinkIOS).toBeCalled();
   });
 
-  it("android", () => {
-    expect("").toMatch("");
+  it("android", async () => {
+    jest.mock(
+      pathk.project.resolve(
+        "node_modules",
+        "react-native-navigation",
+        "autolink",
+        "postlink",
+        "postLinkAndroid.js"
+      )
+    );
+
+    const postLinkAndroid = require(pathk.project.resolve(
+      "node_modules",
+      "react-native-navigation",
+      "autolink",
+      "postlink",
+      "postLinkAndroid.js"
+    ));
+
+    await android();
+
+    expect(postLinkAndroid).toBeCalled();
   });
 });
