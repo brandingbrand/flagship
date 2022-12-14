2ae18bde7fc44f40ef0ad0086268bbefc2642840
Christopher Herman <crherman7@gmail.com>
2022-12-14 17:14:41 -0500

    feat(fs12): FCR-3734 implement google signin plugin (#2440)

--- packages/plugin-google-signin/test/index.test.ts
+++ packages/plugin-google-signin/test/index.test.ts
@@ -1,9 +1,120 @@
+import { path, fs } from "@brandingbrand/kernel-core";
+
+import { ios, android } from "../src";
+
 describe("plugin-google-signin", () => {
-  it("ios", () => {
-    expect("").toMatch("");
+  beforeAll(async () => {
+    return fs.copy(
+      path.resolve(__dirname, "fixtures"),
+      path.resolve(__dirname, "__google_fixtures")
+    );
   });
 
-  it("android", () => {
-    expect("").toMatch("");
+  afterAll(async () => {
+    return fs.remove(path.resolve(__dirname, "__google_fixtures"));
+  });
+
+  it("ios", async () => {
+    jest
+      .spyOn(path.ios, "infoPlistPath")
+      .mockReturnValue(
+        path.resolve(__dirname, "__google_fixtures", "Info.plist")
+      );
+
+    jest
+      .spyOn(path.ios, "appDelegatePath")
+      .mockReturnValue(
+        path.resolve(__dirname, "__google_fixtures", "AppDelegate.mm")
+      );
+
+    await ios({
+      kernelPluginGoogleSignin: {
+        kernel: {
+          ios: {
+            reversedClientId: "kernel-app",
+          },
+          android: {},
+        },
+      },
+    });
+
+    expect(
+      (
+        await fs.readFile(
+          path.resolve(__dirname, "__google_fixtures", "AppDelegate.mm")
+        )
+      ).toString()
+    ).toMatch(
+      '#import <RNGoogleSignin/RNGoogleSignin.h>\n#import "RNGoogleSignin.h"'
+    );
+
+    expect(
+      (
+        await fs.readFile(
+          path.resolve(__dirname, "__google_fixtures", "AppDelegate.mm")
+        )
+      ).toString()
+    ).toMatch(
+      "[RNGoogleSignin application:application openURL:url options:options]"
+    );
+
+    expect(
+      (
+        await fs.readFile(
+          path.resolve(__dirname, "__google_fixtures", "Info.plist")
+        )
+      ).toString()
+    ).toMatch("CFBundleURLSchemes");
+
+    expect(
+      (
+        await fs.readFile(
+          path.resolve(__dirname, "__google_fixtures", "Info.plist")
+        )
+      ).toString()
+    ).toMatch("<string>kernel-app</string>");
+  });
+
+  it("android", async () => {
+    jest
+      .spyOn(path.android, "gradlePath")
+      .mockReturnValue(
+        path.resolve(__dirname, "__google_fixtures", "mbuild.gradle")
+      );
+
+    jest
+      .spyOn(path.project, "resolve")
+      .mockReturnValue(
+        path.resolve(__dirname, "__google_fixtures", "pbuild.gradle")
+      );
+
+    await android({
+      kernelPluginGoogleSignin: {
+        kernel: {
+          ios: {
+            reversedClientId: "kernel-app",
+          },
+          android: {},
+        },
+      },
+    });
+
+    expect(
+      (
+        await fs.readFile(
+          path.resolve(__dirname, "__google_fixtures", "mbuild.gradle")
+        )
+      ).toString()
+    ).toMatch(
+      "implementation 'androidx.swiperefreshlayout:swiperefreshlayout:1.0.0'"
+    );
+
+    expect(
+      (
+        await fs.readFile(
+          path.resolve(__dirname, "__google_fixtures", "pbuild.gradle")
+        )
+      ).toString()
+    ).toMatch('googlePlayServicesAuthVersion = "19.2.0"');
   });
 });
