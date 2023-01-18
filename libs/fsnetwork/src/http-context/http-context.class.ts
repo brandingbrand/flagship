f05122c95b1098f0c9b817420b853462b1a2879d
Stefan Neamtu <stefan.neamtu@brandingbrand.com>
2023-01-18 13:22:42 -0500

    fix(fsnetwork): cors proxy not working with dynamically delivered components
    - change the storage map to store the token name (string) instead of the token to make conditional checks behave properly
    
    brandingbrand-source-id: 5f235bb9308abb133a11761e7e0783284bd43c61

--- libs/fsnetwork/src/http-context/http-context.class.ts
+++ libs/fsnetwork/src/http-context/http-context.class.ts
@@ -1,27 +1,36 @@
 import type { HttpContextToken } from './http-context.token';
 
 export class HttpContext {
-  private readonly storage = new Map<HttpContextToken, unknown>();
+  private readonly storage = new Map<HttpContextToken['name'], unknown>();
+
+  private setWithName<T>(name: HttpContextToken<T>['name'], value: T): this {
+    this.storage.set(name, value);
+    return this;
+  }
+
+  private getWithName<T>(name: HttpContextToken<T>['name']): T | undefined {
+    return this.storage.get(name) as T | undefined;
+  }
 
   public set<T>(token: HttpContextToken<T>, value: T): this {
-    this.storage.set(token, value);
+    this.storage.set(token.name, value);
     return this;
   }
 
   public get<T>(token: HttpContextToken<T>): T {
-    return (this.storage.get(token) as T | undefined) ?? token.defaultValue;
+    return (this.storage.get(token.name) as T | undefined) ?? token.defaultValue;
   }
 
   public delete(token: HttpContextToken): this {
-    this.storage.delete(token);
+    this.storage.delete(token.name);
     return this;
   }
 
   public has(token: HttpContextToken): boolean {
-    return this.storage.has(token);
+    return this.storage.has(token.name);
   }
 
-  public keys(): IterableIterator<HttpContextToken> {
+  public keys(): IterableIterator<HttpContextToken['name']> {
     return this.storage.keys();
   }
 
@@ -29,11 +38,11 @@ export class HttpContext {
     const mergedContext = new HttpContext();
 
     for (const key of this.keys()) {
-      mergedContext.set(key, this.get(key));
+      mergedContext.setWithName(key, this.getWithName(key));
     }
 
     for (const key of context.keys()) {
-      mergedContext.set(key, context.get(key));
+      mergedContext.setWithName(key, context.getWithName(key));
     }
 
     return mergedContext;
