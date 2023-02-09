5c0e5e794fdfab9bf06e268fb40c647451447c66
Christopher Herman <crherman7@gmail.com>
2023-02-09 10:17:29 -0500

    feat(fs12): xml parser (#2507)

--- packages/core/src/executors/init/postPlatform/index.ts
+++ packages/core/src/executors/init/postPlatform/index.ts
@@ -1,5 +1,6 @@
+import * as manifest from "./manifest";
 import * as plist from "./plist";
 import * as plugins from "./plugins";
 import * as cocoapods from "./cocoapods";
 
-export const executors = [plist, plugins, cocoapods];
+export const executors = [manifest, plist, plugins, cocoapods];
---
+++ packages/core/src/utils/xml.ts
@@ -0,0 +1,33 @@
+import { X2jOptionsOptional, XMLBuilder, XMLParser } from "fast-xml-parser";
+
+import fs from "./fs";
+
+const OPTS = {
+  ignoreAttributes: false,
+  attributeNamePrefix: "",
+  attributesGroupName: "$",
+};
+
+const BUILD_OPTS = {
+  ...OPTS,
+  format: true,
+  indentBy: "    ",
+  suppressBooleanAttributes: false,
+  suppressEmptyNode: true,
+};
+
+export const withXml = async <T>(
+  path: string,
+  options: X2jOptionsOptional,
+  callback: (xml: T) => void
+) => {
+  const contents = await fs.promises.readFile(path);
+  const xml = new XMLParser({
+    ...OPTS,
+    ...options,
+  }).parse(contents);
+
+  callback(xml);
+
+  await fs.promises.writeFile(path, new XMLBuilder(BUILD_OPTS).build(xml));
+};
