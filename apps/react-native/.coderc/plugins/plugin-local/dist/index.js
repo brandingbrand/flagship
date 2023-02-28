26b47759f11d8fd08c512ba7a38aae968767cff9
Chris Herman <crherman7@gmail.com>
2023-02-28 10:57:57 -0500

    chore(fs12): decompress bundles

--- apps/react-native/.coderc/plugins/plugin-local/dist/index.js
+++ apps/react-native/.coderc/plugins/plugin-local/dist/index.js
@@ -1,1 +1,11 @@
-var r=require("@brandingbrand/code-core");const a=r.summary.withSummary(async r=>{},"plugin-local","platform::ios"),o=r.summary.withSummary(async r=>{},"plugin-local","platform::android");exports.android=o,exports.ios=a;
+var codeCore = require('@brandingbrand/code-core');
+
+const ios = codeCore.summary.withSummary(async config => {
+  //
+}, 'plugin-local', 'platform::ios');
+const android = codeCore.summary.withSummary(async config => {
+  //
+}, 'plugin-local', 'platform::android');
+
+exports.android = android;
+exports.ios = ios;
