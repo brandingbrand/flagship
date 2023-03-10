b81bbfd69fcaeee4b15711b3f8ec4f75b6a56f1e
Jason Mosley <jmosley@pagewaiter.com>
2023-03-10 12:15:06 -0500

    fix(docs): updating astro config for deployments

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -8,5 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  site: "https://brandingbrand.github.io",
+  base: "/flagship",
+  integrations: [tailwind(), mdx()],
 });
