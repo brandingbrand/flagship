d2121c3e665036538147e3f297f44458ac7913cc
Jason Mosley <jmosley@pagewaiter.com>
2023-03-10 12:54:55 -0500

    fix(docs): adding base to all urls

--- docs/src/lib/config.ts
+++ docs/src/lib/config.ts
@@ -1,49 +1,58 @@
 export const SIDEBAR = {
   Code: [
-    { text: "Introduction", link: "/en/introduction" },
-    { text: "FAQ", link: "/en/faq" },
-    { text: "Roadmap", link: "/en/roadmap" },
+    { text: "Introduction", link: "/flagship/en/introduction" },
+    { text: "FAQ", link: "/flagship/en/faq" },
+    { text: "Roadmap", link: "/flagship/en/roadmap" },
   ],
   Packages: [
-    { text: "core", link: "/en/packages/core" },
-    { text: "cli", link: "/en/packages/cli" },
-    { text: "plugin-app-icon", link: "/en/packages/plugins/app-icon" },
-    { text: "plugin-asset", link: "/en/packages/plugins/asset" },
-    { text: "plugin-fastlane", link: "/en/packages/plugins/fastlane" },
-    { text: "plugin-fbsdk-next", link: "/en/packages/plugins/fbsdk-next" },
+    { text: "core", link: "/flagship/en/packages/core" },
+    { text: "cli", link: "/flagship/en/packages/cli" },
+    { text: "plugin-app-icon", link: "/flagship/en/packages/plugins/app-icon" },
+    { text: "plugin-asset", link: "/flagship/en/packages/plugins/asset" },
+    { text: "plugin-fastlane", link: "/flagship/en/packages/plugins/fastlane" },
+    {
+      text: "plugin-fbsdk-next",
+      link: "/flagship/en/packages/plugins/fbsdk-next",
+    },
     {
       text: "plugin-firebase-analytics",
-      link: "/en/packages/plugins/firebase-analytics",
+      link: "/flagship/en/packages/plugins/firebase-analytics",
     },
-    { text: "plugin-firebase-app", link: "/en/packages/plugins/firebase-app" },
+    {
+      text: "plugin-firebase-app",
+      link: "/flagship/en/packages/plugins/firebase-app",
+    },
     {
       text: "plugin-google-signin",
-      link: "/en/packages/plugins/google-signin",
+      link: "/flagship/en/packages/plugins/google-signin",
     },
-    { text: "plugin-leanplum", link: "/en/packages/plugins/leanplum" },
+    { text: "plugin-leanplum", link: "/flagship/en/packages/plugins/leanplum" },
     {
       text: "plugin-native-navigation",
-      link: "/en/packages/plugins/native-navigation",
+      link: "en/packages/plugins/native-navigation",
     },
-    { text: "plugin-permissions", link: "/en/packages/plugins/permissions" },
+    {
+      text: "plugin-permissions",
+      link: "/flagship/en/packages/plugins/permissions",
+    },
     {
       text: "plugin-splash-screen",
-      link: "/en/packages/plugins/splash-screen",
+      link: "/flagship/en/packages/plugins/splash-screen",
     },
     {
       text: "plugin-target-application",
-      link: "/en/packages/plugins/target-application",
+      link: "/flagship/en/packages/plugins/target-application",
     },
     {
       text: "plugin-target-extension",
-      link: "/en/packages/plugins/target-extension",
+      link: "en/packages/plugins/target-extension",
     },
   ],
   Usage: [
-    { text: "Integration", link: "/en/usage/integration" },
-    { text: "Contribution", link: "/en/usage/contribution" }, 
-    { text: "Troubleshooting", link: "/en/usage/troubleshooting" },
+    { text: "Integration", link: "/flagship/en/usage/integration" },
+    { text: "Contribution", link: "/flagship/en/usage/contribution" },
+    { text: "Troubleshooting", link: "/flagship/en/usage/troubleshooting" },
   ],
 };
 
-export const IGNORE_DIRS = ["packages", "plugins", "usage"];
+export const IGNORE_DIRS = ["flagship", "packages", "plugins", "usage"];
