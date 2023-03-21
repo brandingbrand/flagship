d1d8d7710800e5bba5d4ff5172cc98f6f6514c86
Chris Herman <crherman7@gmail.com>
2023-03-21 13:41:48 -0400

    chore(fs12): move ejs template -> assets

---
+++ packages/core/assets/template/ios/{%= ios.name %}/NativeConstants.m
@@ -0,0 +1,23 @@
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface NativeConstants : NSObject <RCTBridgeModule>
+@end
+
+
+@implementation NativeConstants
+RCT_EXPORT_MODULE();
+
++ (BOOL)requiresMainQueueSetup
+{
+    return NO;
+}
+
+- (NSDictionary *)constantsToExport {
+  return @{
+    @"installed": @YES,
+    @"ShowDevMenu": @"<%- !release %>"
+  };
+}
+
+@end
