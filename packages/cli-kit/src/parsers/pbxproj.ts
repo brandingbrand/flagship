a49fe64f254937cf7042f0896fa7d1f83830e42a
Chris Herman <crherman7@gmail.com>
2024-01-30 17:15:06 -0500

    feat(code): add docs for pbxproj parser

--- packages/cli-kit/src/parsers/pbxproj.ts
+++ packages/cli-kit/src/parsers/pbxproj.ts
@@ -1,15 +1,46 @@
 import fs from "fs/promises";
-import xcode, { type XcodeProject } from "xcode";
+import xcode, { XcodeProject } from "xcode";
 
 import { paths } from "@/lib";
 
+/**
+ * Reads an Xcode project file, applies a callback function to the parsed Xcode project,
+ * and writes the updated project back to the same file.
+ *
+ * @param {Function} callback - The callback function applied to the Xcode project.
+ * @returns {Promise<void>} A Promise that resolves once the operation is complete.
+ *
+ * @example
+ * ```typescript
+ * await withPbxproj(async (project) => {
+ *   // Modify the Xcode project using the provided 'project' instance
+ *   project.addTarget(...);
+ * });
+ * ```
+ */
 export async function withPbxproj(
   callback: (pbxproj: XcodeProject) => Promise<void>
-) {
+): Promise<void> {
+  /**
+   * Creates an Xcode project instance by parsing the content of the project file.
+   *
+   * @returns {XcodeProject} An instance of the parsed Xcode project.
+   */
   const project = xcode.project(paths.ios.projectPbxProj());
   project.parseSync();
 
-  callback(project);
+  /**
+   * Applies the callback function to the parsed Xcode project.
+   *
+   * @param {XcodeProject} project - The Xcode project instance.
+   * @returns {Promise<void>} A Promise that resolves once the callback operation is complete.
+   */
+  await callback(project);
 
+  /**
+   * Writes the updated Xcode project back to the project file.
+   *
+   * @returns {Promise<void>} A Promise that resolves once the write operation is complete.
+   */
   await fs.writeFile(paths.ios.projectPbxProj(), project.writeSync());
 }
