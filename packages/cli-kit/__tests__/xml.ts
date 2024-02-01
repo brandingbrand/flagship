a0ddd8bea7df148b20037354af56583cff8f26f1
Chris Herman <crherman7@gmail.com>
2024-02-01 17:23:00 -0500

    feat(code): update test paths

--- packages/cli-kit/__tests__/xml.ts
+++ packages/cli-kit/__tests__/xml.ts
@@ -11,26 +11,23 @@ import {
   withStyles,
   withXml,
 } from "../src/parsers/xml";
+import { paths } from "../src/lib/paths";
 
 jest.mock("fs/promises");
 jest.mock("fast-xml-parser");
 
-const path = "/path/to/xml";
 const originalXml = { root: { element: "value" } };
 const modifiedXml = { root: { element: "modifiedValue" } };
 
 jest.spyOn(fs, "readFile").mockResolvedValue(JSON.stringify(originalXml));
-jest.spyOn(XMLParser.prototype, "parse").mockReturnValue(originalXml);
 jest.spyOn(fs, "writeFile").mockResolvedValue();
+jest.spyOn(XMLParser.prototype, "parse").mockReturnValue(originalXml);
 
 describe("withXml", () => {
   it("should parse, modify, and write XML file", async () => {
     const path = "/path/to/xml";
-    const originalXml = { root: { element: "value" } };
-    const modifiedXml = { root: { element: "modifiedValue" } };
-    const options: X2jOptions = {};
 
-    await withXml(path, options, (xml: any) => {
+    await withXml(path, {}, (xml: any) => {
       expect(xml).toEqual(originalXml);
 
       xml.root.element = "modifiedValue";
@@ -38,7 +35,6 @@ describe("withXml", () => {
 
     expect(fs.readFile).toHaveBeenCalledWith(path);
     expect(XMLParser).toHaveBeenCalledWith({
-      ...options,
       ...OPTS,
     });
     expect(fs.writeFile).toHaveBeenCalledWith(
@@ -56,9 +52,9 @@ describe("withColors", () => {
       xml.root.element = "modifiedValue";
     });
 
-    expect(fs.readFile).toHaveBeenCalledWith(path);
+    expect(fs.readFile).toHaveBeenCalledWith(paths.android.colors());
     expect(fs.writeFile).toHaveBeenCalledWith(
-      path,
+      paths.android.colors(),
       new XMLBuilder({ ...BUILD_OPTS, format: true }).build(modifiedXml)
     );
   });
@@ -72,9 +68,9 @@ describe("withStyles", () => {
       xml.root.element = "modifiedValue";
     });
 
-    expect(fs.readFile).toHaveBeenCalledWith(path);
+    expect(fs.readFile).toHaveBeenCalledWith(paths.android.styles());
     expect(fs.writeFile).toHaveBeenCalledWith(
-      path,
+      paths.android.styles(),
       new XMLBuilder({ ...BUILD_OPTS, format: true }).build(modifiedXml)
     );
   });
@@ -88,9 +84,9 @@ describe("withManifest", () => {
       xml.root.element = "modifiedValue";
     });
 
-    expect(fs.readFile).toHaveBeenCalledWith(path);
+    expect(fs.readFile).toHaveBeenCalledWith(paths.android.androidManifest());
     expect(fs.writeFile).toHaveBeenCalledWith(
-      path,
+      paths.android.androidManifest(),
       new XMLBuilder({ ...BUILD_OPTS, format: true }).build(modifiedXml)
     );
   });
@@ -101,12 +97,14 @@ describe("withNetworkSecurityConfig", () => {
     await withNetworkSecurityConfig((xml: any) => {
       expect(xml).toEqual(originalXml);
 
-      xml.root.element = "modifiedValue";
+      xml.root.element = "modifiedValue1";
     });
 
-    expect(fs.readFile).toHaveBeenCalledWith(path);
+    expect(fs.readFile).toHaveBeenCalledWith(
+      paths.android.networkSecurityConfig()
+    );
     expect(fs.writeFile).toHaveBeenCalledWith(
-      path,
+      paths.android.networkSecurityConfig(),
       new XMLBuilder({ ...BUILD_OPTS, format: true }).build(modifiedXml)
     );
   });
@@ -120,9 +118,9 @@ describe("withStrings", () => {
       xml.root.element = "modifiedValue";
     });
 
-    expect(fs.readFile).toHaveBeenCalledWith(path);
+    expect(fs.readFile).toHaveBeenCalledWith(paths.android.strings());
     expect(fs.writeFile).toHaveBeenCalledWith(
-      path,
+      paths.android.strings(),
       new XMLBuilder({ ...BUILD_OPTS, format: true }).build(modifiedXml)
     );
   });
