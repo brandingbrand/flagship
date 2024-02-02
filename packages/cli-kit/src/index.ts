d8744b7f0ecff3b20e93f49107ecc2eee85b5d79
Chris Herman <crherman7@gmail.com>
2024-02-02 14:27:30 -0500

    feat(code): index comments

--- packages/cli-kit/src/index.ts
+++ packages/cli-kit/src/index.ts
@@ -1,3 +1,24 @@
+/**
+ * CliKit module serves as entry point for the cli-kit
+ *
+ * @module cli-kit
+ */
+
+/**
+ * Lib dir serves as entry the point of the lib modules
+ *
+ * @dir cli-kit/lib
+ */
 export * from "./lib";
+
+/**
+ * Parsers dir serves as the entry point of the parsers module
+ *
+ * @dir cli-kit/parsers
+ */
 export * from "./parsers";
+
+/**
+ * @Types dir serves as the entry point of the @types module
+ */
 export * from "./@types";
