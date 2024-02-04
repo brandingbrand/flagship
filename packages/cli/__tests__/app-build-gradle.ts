d1465b516f495dedaf2d6a3e24213b3d12e5942b
Chris Herman <crherman7@gmail.com>
2024-02-04 17:44:47 -0500

    feat(code): module build.gradle transformers and unit tests

---
+++ packages/cli/__tests__/app-build-gradle.ts
@@ -0,0 +1,51 @@
+/**
+ * @jest-environment-options {"requireTemplate": true}
+ */
+
+/// <reference types="@brandingbrand/code-jest-config/@types/globals.d.ts" />
+
+import { type BuildConfig, fs, path } from "@brandingbrand/code-cli-kit";
+
+import transformer from "../src/transformers/android/app-build-gradle";
+
+describe("build.gradle transformers", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should update namespace, applicationId, versionCode and versionName", async () => {
+    const config = {
+      ...__flagship_code_build_config,
+    } as BuildConfig;
+
+    config.android.versioning = {
+      build: 3,
+      version: "1.3",
+    };
+
+    await transformer.transform(config);
+    const content = await fs.readFile(path.android.appBuildGradle, "utf-8");
+
+    expect(content).toContain('namespace "com.brandingbrand"');
+    expect(content).toContain('applicationId "com.brandingbrand"');
+    expect(content).toContain("versionCode 3");
+    expect(content).toContain('versionName "1.3"');
+  });
+
+  it("should add dependency to dependencies object", async () => {
+    const config = {
+      ...__flagship_code_build_config,
+    } as BuildConfig;
+    config.android.gradle = {
+      appGradle: {
+        dependencies: ['classpath("com.brandingbrand.tools.build:gradle")'],
+      },
+    };
+
+    await transformer.transform(config);
+    const content = await fs.readFile(path.android.appBuildGradle, "utf-8");
+
+    expect(content).toContain(`dependencies {
+    classpath("com.brandingbrand.tools.build:gradle")`);
+  });
+});
--- packages/cli/src/transformers/android/app-build-gradle.ts
+++ packages/cli/src/transformers/android/app-build-gradle.ts
@@ -1,26 +1,164 @@
-import { type BuildConfig, withUTF8, path } from "@brandingbrand/code-cli-kit";
+import {
+  type BuildConfig,
+  withUTF8,
+  path,
+  string,
+} from "@brandingbrand/code-cli-kit";
 
 import { defineTransformer } from "@/lib";
 
 /**
  * Defines a transformer for the Android project's "app-build.gradle" file.
  *
- * @type {typeof defineTransformer<(content: string, config: BuildConfig) => string>} - The type of the transformer.
- * @property {string} file - The name of the file to be transformed ("build.gradle").
+ * @typedef {Object} AndroidAppBuildGradleTransformer - Android app-build.gradle transformer configuration.
+ * @property {string} file - The name of the file to be transformed ("app-build.gradle").
  * @property {Array<(content: string, config: BuildConfig) => string>} transforms - An array of transformer functions.
  * @property {Function} transform - The main transform function that applies all specified transformations.
- * @returns {Promise<string>} The updated content of the "build.gradle" file.
+ * @returns {Promise<string>} The updated content of the "app-build.gradle" file.
+ */
+
+/**
+ * @type {typeof defineTransformer}
+ * @template {(content: string, config: BuildConfig) => string} T - Type of transformations to be applied.
+ * @param {AndroidAppBuildGradleTransformer} transformerConfig - Configuration for the Android app-build.gradle transformer.
+ * @returns {Transforms<string>} - The type of the transformer.
  */
 export default defineTransformer<
   (content: string, config: BuildConfig) => string
 >({
+  /**
+   * The name of the file to be transformed ("app-build.gradle").
+   * @type {string}
+   */
   file: "app-build.gradle",
+
+  /**
+   * An array of transformer functions to be applied to the "app-build.gradle" file.
+   * Each function receives the content of the file and the build configuration,
+   * and returns the updated content after applying specific transformations.
+   * @type {Array<(content: string, config: BuildConfig) => string>}
+   */
   transforms: [
+    /**
+     * Transformer for updating the "namespace" value in "app-build.gradle".
+     * @param {string} content - The content of the file.
+     * @param {BuildConfig} config - The build configuration.
+     * @returns {string} - The updated content.
+     */
+    (content: string, config: BuildConfig): string => {
+      return string.replace(
+        content,
+        /(namespace\s*")(\w|\.)+(")/m,
+        `$1${config.android.packageName}$3`
+      );
+    },
+
+    /**
+     * Transformer for updating the "applicationId" value in "app-build.gradle".
+     * @param {string} content - The content of the file.
+     * @param {BuildConfig} config - The build configuration.
+     * @returns {string} - The updated content.
+     */
+    (content: string, config: BuildConfig): string => {
+      return string.replace(
+        content,
+        /(applicationId\s*")(\w|\.)+(")/m,
+        `$1${config.android.packageName}$3`
+      );
+    },
+
+    /**
+     * Transformer for updating the "versionCode" value in "app-build.gradle".
+     * @param {string} content - The content of the file.
+     * @param {BuildConfig} config - The build configuration.
+     * @returns {string} - The updated content.
+     */
+    (content: string, config: BuildConfig): string => {
+      if (!config.android.versioning?.build) return content;
+
+      return string.replace(
+        content,
+        /(versionCode\s+)\d+/m,
+        `$1${config.android.versioning?.build}`
+      );
+    },
+
+    /**
+     * Transformer for updating the "versionName" value in "app-build.gradle".
+     * @param {string} content - The content of the file.
+     * @param {BuildConfig} config - The build configuration.
+     * @returns {string} - The updated content.
+     */
+    (content: string, config: BuildConfig): string => {
+      if (!config.android.versioning?.version) return content;
+
+      return string.replace(
+        content,
+        /(versionName\s*")(\w|\.)+(")/m,
+        `$1${config.android.versioning?.version}$3`
+      );
+    },
+
+    /**
+     * Transformer for updating the "signingConfigs" section in "app-build.gradle".
+     * @param {string} content - The content of the file.
+     * @param {BuildConfig} config - The build configuration.
+     * @returns {string} - The updated content.
+     */
     (content: string, config: BuildConfig) => {
-      return "";
+      return string.replace(
+        content,
+        /(signingConfigs\s*{\s*)/m,
+        `$1release {
+            if (project.hasProperty('MYAPP_RELEASE_STORE_FILE')) {
+                storeFile file(MYAPP_RELEASE_STORE_FILE)
+                storePassword MYAPP_RELEASE_STORE_PASSWORD
+                keyAlias MYAPP_RELEASE_KEY_ALIAS
+                keyPassword MYAPP_RELEASE_KEY_PASSWORD
+            }
+        }
+        `
+      );
+    },
+
+    /**
+     * Transformer for updating the "buildTypes" section in "app-build.gradle".
+     * @param {string} content - The content of the file.
+     * @param {BuildConfig} config - The build configuration.
+     * @returns {string} - The updated content.
+     */
+    (content: string, config: BuildConfig) => {
+      return string.replace(
+        content,
+        /(buildTypes\s*{[\s\S]*release\s*{[\s\S]*signingConfig\s+).*/m,
+        "$1signingConfigs.release"
+      );
+    },
+
+    /**
+     * Transformer for updating the "dependencies" section in "app-build.gradle".
+     * @param {string} content - The content of the file.
+     * @param {BuildConfig} config - The build configuration.
+     * @returns {string} - The updated content.
+     */
+    (content: string, config: BuildConfig): string => {
+      if (config.android.gradle?.appGradle?.dependencies === undefined)
+        return content;
+
+      return string.replace(
+        content,
+        /(dependencies\s*{[\s\S]\s+)/m,
+        `$1${config.android.gradle.appGradle.dependencies.map((it) => `${it}\n    `).join("")}`
+      );
     },
   ],
-  transform: async function (config: BuildConfig) {
+
+  /**
+   * The main transform function that applies all specified transformations to the "app-build.gradle" file.
+   * @param {BuildConfig} config - The build configuration.
+   * @returns {Promise<void>} - The updated content of the "app-build.gradle" file.
+   */
+  transform: async function (config: BuildConfig): Promise<void> {
     return withUTF8(path.android.appBuildGradle, (content: string) => {
       return this.transforms.reduce((acc, curr) => {
         return curr(acc, config);
