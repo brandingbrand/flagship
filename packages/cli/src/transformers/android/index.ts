2a42d6fa20e80772d0f3fa37c15b0674bf6d4222
Chris Herman <crherman7@gmail.com>
2024-02-06 11:50:13 -0500

    feat(code): add android transformer exports

--- packages/cli/src/transformers/android/index.ts
+++ packages/cli/src/transformers/android/index.ts
@@ -1,10 +1,49 @@
+/**
+ * Represents the AndroidManifest XML file transformers.
+ */
 export { default as androidManifest } from "./android-manifest-xml";
+
+/**
+ * Represents the app build.gradle file transformers.
+ */
 export { default as appBuildGradle } from "./app-build-gradle";
+
+/**
+ * Represents the build.gradle file transformers.
+ */
 export { default as buildGradle } from "./build-gradle";
-export * from "./colors-xml";
+
+/**
+ * Represents the colors XML file transformers.
+ */
+export { default as colors } from "./colors-xml";
+
+/**
+ * Represents the Gemfile transformers.
+ */
 export { default as gemfile } from "./gemfile";
+
+/**
+ * Represents the gradle.properties file transformers.
+ */
 export { default as gradleProperties } from "./gradle-properties";
+
+/**
+ * Represents the environment switcher in Java transformers.
+ */
 export { default as EnvSwitcher } from "./env-switcher-java";
+
+/**
+ * Represents the native constants in Java transformers.
+ */
 export { default as nativeConstants } from "./native-constants-java";
-export * from "./strings-xml";
-export * from "./styles-xml";
+
+/**
+ * Represents the strings XML file transformers.
+ */
+export { default as strings } from "./strings-xml";
+
+/**
+ * Represents the styles XML file transformers.
+ */
+export { default as styles } from "./styles-xml";
