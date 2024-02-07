ee298249821c98a9f29b5b3c7cc9df128b1d8f12
Chris Herman <crherman7@gmail.com>
2024-02-07 17:45:24 -0500

    feat(code): update errors isWarning unit tests

--- packages/cli/__tests__/errors.ts
+++ packages/cli/__tests__/errors.ts
@@ -28,27 +28,6 @@ describe("isWarning function", () => {
     expect(isWarning(error)).toBe(false);
   });
 
-  it("should return false for an object with no 'name' property", () => {
-    const objWithoutName = { message: "This is not an error" };
-
-    expect(isWarning(objWithoutName)).toBe(false);
-  });
-
-  it("should return false for an object with 'name' property of non-string type", () => {
-    const objWithNameNotString = { name: 123, message: "This is not an error" };
-
-    expect(isWarning(objWithNameNotString)).toBe(false);
-  });
-
-  it("should return false for an object with 'name' property not containing 'warning'", () => {
-    const objWithNonWarningName = {
-      name: "Error",
-      message: "This is not an error",
-    };
-
-    expect(isWarning(objWithNonWarningName)).toBe(false);
-  });
-
   it("should return true for a warning error object with 'warning' in the name in any case", () => {
     const warningError = new Error("This is a WARNING");
     warningError.name = "myWARNING";
