db015edc91ab69b84523d830feb48ef76b826819
Chris Herman <crherman7@gmail.com>
2024-02-09 14:09:23 -0500

    feat(code): add docs to flagship-code.config.ts configuration file

--- apps/example/flagship-code.config.ts
+++ apps/example/flagship-code.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from "@brandingbrand/code-cli-kit";
 
+/**
+ * Defines the configuration for the Code CLI tool.
+ *
+ * @returns {object} The configuration object.
+ */
 export default defineConfig({
+  /**
+   * The path to the build directory.
+   */
   buildPath: "./coderc/build",
+
+  /**
+   * The path to the environment directory.
+   */
   envPath: "./coderc/env",
+
+  /**
+   * The path to the plugin directory.
+   */
   pluginPath: "./coderc/plugins",
+
+  /**
+   * An array of plugin names.
+   */
   plugins: [],
 });
