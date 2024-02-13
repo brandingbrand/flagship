c98d0f8fecbde7573356f81cc1de01a845420574
Chris Herman <crherman7@gmail.com>
2024-02-13 13:50:43 -0500

    feat(code): add base example app

---
+++ apps/example/metro.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  watchFolders: [path.resolve(__dirname, '../../node_modules')],
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
