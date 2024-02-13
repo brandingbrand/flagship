62016bd2de9b0a189464cda9f40a328bdb78ef9d
Chris Herman <crherman7@gmail.com>
2024-02-13 17:03:49 -0500

    feat(code): add code-plugin-native-navigation unit tests

---
+++ packages/plugin-native-navigation/__tests__/index.ts
@@ -0,0 +1,33 @@
+import plugin from "../src";
+
+describe("plugin-native-navigation android", () => {
+  const rnnLinkIOS = require.resolve(
+    "react-native-navigation/autolink/postlink/postLinkIOS.js",
+    { paths: [process.cwd()] }
+  );
+
+  const rnnLinkAndroid = require.resolve(
+    "react-native-navigation/autolink/postlink/postLinkAndroid.js",
+    { paths: [process.cwd()] }
+  );
+
+  it("ios", async () => {
+    jest.mock(rnnLinkIOS, () => jest.fn().mockResolvedValue(undefined));
+
+    await plugin.ios?.({} as any, {} as any);
+
+    const postLinkIOS = require(rnnLinkIOS);
+
+    expect(postLinkIOS).toHaveBeenCalled();
+  });
+
+  it("android", async () => {
+    jest.mock(rnnLinkAndroid, () => jest.fn().mockResolvedValue(undefined));
+
+    await plugin.android?.({} as any, {} as any);
+
+    const postLinkAndroid = require(rnnLinkAndroid);
+
+    expect(postLinkAndroid).toHaveBeenCalled();
+  });
+});
--- packages/plugin-native-navigation/src/index.ts
+++ packages/plugin-native-navigation/src/index.ts
@@ -1,26 +1,69 @@
+/**
+ * Defines a plugin for @brandingbrand/code-cli-kit.
+ * @module Plugin
+ */
+
 import { definePlugin, fs } from "@brandingbrand/code-cli-kit";
 
+/**
+ * Defines a plugin with functions for both iOS and Android platforms.
+ * @alias module:Plugin
+ * @param {Object} build - The build configuration object.
+ * @param {Object} options - The options object.
+ */
 export default definePlugin({
+  /**
+   * Function to be executed for iOS platform.
+   * @param {Object} build - The build configuration object for iOS.
+   * @param {Object} options - The options object for iOS.
+   * @returns {Promise<void>} A promise that resolves when the process completes.
+   */
   ios: async function (build, options) {
+    // Resolve path to react-native-navigation postlink path module
+    const rnnPath = require.resolve(
+      "react-native-navigation/autolink/postlink/path.js",
+      { paths: [process.cwd()] }
+    );
+
+    // Update mainApplicationJava in postlink path module
+    await fs.update(rnnPath, /(mainApplicationJava)\S*(replace)/, `$1?.$2`);
+
+    // Resolve path to react-native-navigation postlink IOS script
     const scriptPath = require.resolve(
       "react-native-navigation/autolink/postlink/postLinkIOS.js",
       { paths: [process.cwd()] }
     );
 
+    // Set executable permission for postlink IOS script
     await fs.chmod(scriptPath, "755");
+
+    // Require postlink IOS script
     const rnnIOSLink = require(scriptPath);
 
+    // Execute postlink IOS script
     await rnnIOSLink();
   },
+
+  /**
+   * Function to be executed for Android platform.
+   * @param {Object} build - The build configuration object for Android.
+   * @param {Object} options - The options object for Android.
+   * @returns {Promise<void>} A promise that resolves when the process completes.
+   */
   android: async function (build, options) {
+    // Resolve path to react-native-navigation postlink Android script
     const scriptPath = require.resolve(
       "react-native-navigation/autolink/postlink/postLinkAndroid.js",
       { paths: [process.cwd()] }
     );
 
+    // Set executable permission for postlink Android script
     await fs.chmod(scriptPath, "755");
+
+    // Require postlink Android script
     const rnnAndroidLink = require(scriptPath);
 
+    // Execute postlink Android script
     await rnnAndroidLink();
   },
 });
