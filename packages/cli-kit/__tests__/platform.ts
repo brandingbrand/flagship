748f09ccf1d73e06a9014e5c2b448a4d13e22327
Chris Herman <crherman7@gmail.com>
2024-03-25 15:32:53 -0400

    fix(code): cli-kit platform unit tests

--- packages/cli-kit/__tests__/platform.ts
+++ packages/cli-kit/__tests__/platform.ts
@@ -5,6 +5,7 @@ import {
   canRunIOS,
   canRunAndroid,
 } from "../src/lib/platform";
+import * as platform from "../src/lib/platform";
 import type { PrebuildOptions } from "../src/@types/config";
 
 describe("Operating System Utilities", () => {
@@ -27,6 +28,20 @@ describe("Operating System Utilities", () => {
   });
 
   describe("canRunIOS", () => {
+    let origionalIsOSX = isOSX;
+
+    beforeAll(() => {
+      Object.defineProperty(platform, "isOSX", {
+        value: true,
+      });
+    });
+
+    afterAll(() => {
+      Object.defineProperty(platform, "isOSX", {
+        value: origionalIsOSX,
+      });
+    });
+
     it('should return true for macOS and platform "ios"', () => {
       const options = { platform: "ios" } as PrebuildOptions;
       expect(canRunIOS(options)).toBe(true);
@@ -44,6 +59,20 @@ describe("Operating System Utilities", () => {
   });
 
   describe("canRunAndroid", () => {
+    let origionalIsOSX = isOSX;
+
+    beforeAll(() => {
+      Object.defineProperty(platform, "isOSX", {
+        value: true,
+      });
+    });
+
+    afterAll(() => {
+      Object.defineProperty(platform, "isOSX", {
+        value: origionalIsOSX,
+      });
+    });
+
     it('should return true for macOS, Linux, or Windows and platform "android"', () => {
       const options = { platform: "android" } as PrebuildOptions;
       expect(canRunAndroid(options)).toBe(true);
