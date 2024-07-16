d708d9c8a790bea1da8560d250aeeb8329667c20
Chris Herman <crherman7@gmail.com>
2024-07-16 13:47:37 -0400

    feat: React Native 0.73 compatibility

---
+++ packages/cli-kit/src/lib/path/path-0.73.ts
@@ -0,0 +1,8 @@
+import pathRN72 from './path-0.72';
+
+export default {
+  ...pathRN72,
+  android: {
+    ...pathRN72.android,
+  },
+} as typeof pathRN72;
--- packages/cli-kit/src/lib/platform.ts
+++ packages/cli-kit/src/lib/platform.ts
@@ -1,3 +1,5 @@
+import semver from 'semver';
+
 import type {PrebuildOptions} from '@/@types';
 
 /**
@@ -36,4 +38,48 @@ export function canRunAndroid(options: PrebuildOptions): boolean {
     (isOSX || isLinux || isWindows) &&
     (options.platform === 'android' || options.platform === 'native')
   );
+}
+
+/**
+ * Gets the major and minor version of the installed React Native package.
+ *
+ * @returns {string} The major and minor version of React Native, in the format 'X.Y'.
+ * @throws Will throw an error if the version is undefined or if the major or minor version cannot be determined.
+ *
+ * @example
+ * ```typescript
+ * const reactNativeVersion = getReactNativeVersion();
+ * console.log(reactNativeVersion); // Example output: "0.72"
+ * ```
+ */
+export function getReactNativeVersion(): string {
+  // Import the version from the React Native package.json
+  const {version} = require('react-native/package.json');
+
+  // Check if the version is undefined and throw an error if it is
+  if (!version) {
+    throw Error(
+      'Type Mismatch: react-native version is expected to be defined.',
+    );
+  }
+
+  // Coerce the version into a SemVer object
+  const coercedVersion = semver.coerce(version);
+
+  // Check if the major version is undefined and throw an error if it is
+  if (!coercedVersion?.major) {
+    throw Error(
+      'Type Mismatch: react-native major version is expected to be defined.',
+    );
+  }
+
+  // Check if the minor version is undefined and throw an error if it is
+  if (!coercedVersion?.minor) {
+    throw Error(
+      'Type Mismatch: react-native minor version is expected to be defined.',
+    );
+  }
+
+  // Return the major and minor version in the format 'X.Y'
+  return `${coercedVersion.major}.${coercedVersion.minor}`;
 }
