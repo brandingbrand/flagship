04e06324b98443fc13c381e48c6dce7898bffc9c
Chris Herman <crherman7@gmail.com>
2024-08-08 10:36:29 -0400

    fix: udpate-check log error

--- packages/cli/src/actions/info.ts
+++ packages/cli/src/actions/info.ts
@@ -1,4 +1,3 @@
-import ansiAlign from 'ansi-align';
 import ci from 'ci-info';
 import updateCheck from 'update-check';
 import {isWindows, logger} from '@brandingbrand/code-cli-kit';
@@ -26,15 +25,19 @@ export default defineAction(async () => {
   }
 
   // Check for package updates
-  const update = await updateCheck({
-    name: pkg.name,
-    version: pkg.version,
-  });
+  try {
+    const update = await updateCheck({
+      name: pkg.name,
+      version: pkg.version,
+    });
 
-  // Warn for new version available
-  if (update) {
-    logger.warn(
-      `A new version of ${pkg.name} is available: v${pkg.version} -> v${update.latest}`,
-    );
+    // Warn for new version available
+    if (update) {
+      logger.warn(
+        `new version of ${pkg.name} is available: v${pkg.version} -> v${update.latest}`,
+      );
+    }
+  } catch (e: any) {
+    logger.error(`failed to check for ${pkg.name} updates: ${e.message}`);
   }
 });
