e3ee5287238ec7476809a1fa133fc1a9c3a1dfc0
Chris Herman <crherman7@gmail.com>
2024-12-12 15:35:24 -0500

    feat(docs): add app-env + app-router docs

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,89 +1,91 @@
-import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
+import {defineConfig} from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 export default defineConfig({
   integrations: [
     starlight({
-      title: "Flagship Code",
-      favicon: "./src/assets/logo.svg",
+      title: 'Flagship Code',
+      favicon: './src/assets/logo.svg',
       logo: {
-        src: "./src/assets/logo.svg",
+        src: './src/assets/logo.svg',
       },
       social: {
-        github: "https://github.com/brandingbrand/flagship",
+        github: 'https://github.com/brandingbrand/flagship',
       },
-      customCss: ["./src/styles.css"],
+      customCss: ['./src/styles.css'],
       sidebar: [
         {
-          label: "Overview",
+          label: 'Overview',
           items: [
-            { label: "Overview", link: "/overview/overview/" },
-            { label: "Objectives", link: "/overview/objectives/" },
-            { label: "How It Works", link: "/overview/how-it-works/" },
+            {label: 'Overview', link: '/overview/overview/'},
+            {label: 'Objectives', link: '/overview/objectives/'},
+            {label: 'How It Works', link: '/overview/how-it-works/'},
           ],
         },
         {
-          label: "Guides",
+          label: 'Guides',
           items: [
             {
-              label: "Getting Started",
-              link: "/guides/getting-started",
+              label: 'Getting Started',
+              link: '/guides/getting-started',
             },
             {
-              label: "Flagship Code Configuration",
-              link: "/guides/config/",
+              label: 'Flagship Code Configuration',
+              link: '/guides/config/',
             },
-            { label: "Build Configuration", link: "/guides/build/" },
-            { label: "Env Configuration", link: "/guides/env/" },
-            { label: "Plugins", link: "/guides/plugins/" },
-            { label: "Migration", link: "/guides/migration/" },
+            {label: 'Build Configuration', link: '/guides/build/'},
+            {label: 'Env Configuration', link: '/guides/env/'},
+            {label: 'Plugins', link: '/guides/plugins/'},
+            {label: 'Migration', link: '/guides/migration/'},
           ],
         },
         {
-          label: "Packages",
+          label: 'Packages',
           items: [
-            { label: "cli", link: "/packages/cli/" },
+            {label: 'app-env', link: '/packages/app-env/'},
+            {label: 'app-router', link: '/packages/app-router/'},
+            {label: 'cli', link: '/packages/cli/'},
             {
-              label: "cli-kit",
-              link: "/packages/cli-kit/",
+              label: 'cli-kit',
+              link: '/packages/cli-kit/',
             },
             {
-              label: "jest-config",
-              link: "/packages/jest-config/",
+              label: 'jest-config',
+              link: '/packages/jest-config/',
             },
             {
-              label: "plugin-app-icon",
-              link: "/packages/plugin-app-icon/",
+              label: 'plugin-app-icon',
+              link: '/packages/plugin-app-icon/',
             },
             {
-              label: "plugin-asset",
-              link: "/packages/plugin-asset/",
+              label: 'plugin-asset',
+              link: '/packages/plugin-asset/',
             },
             {
-              label: "plugin-fastlane",
-              link: "/packages/plugin-fastlane/",
+              label: 'plugin-fastlane',
+              link: '/packages/plugin-fastlane/',
             },
             {
-              label: "plugin-native-navigation",
-              link: "/packages/plugin-native-navigation/",
+              label: 'plugin-native-navigation',
+              link: '/packages/plugin-native-navigation/',
             },
             {
-              label: "plugin-permissions",
-              link: "/packages/plugin-permissions/",
+              label: 'plugin-permissions',
+              link: '/packages/plugin-permissions/',
             },
             {
-              label: "plugin-splash-screen",
-              link: "/packages/plugin-splash-screen/",
+              label: 'plugin-splash-screen',
+              link: '/packages/plugin-splash-screen/',
             },
             {
-              label: "plugin-target-extension",
-              link: "/packages/plugin-target-extension/",
+              label: 'plugin-target-extension',
+              link: '/packages/plugin-target-extension/',
             },
           ],
         },
       ],
     }),
   ],
-  site: "https://brandingbrand.github.io",
-  base: "flagship",
+  site: 'https://brandingbrand.github.io',
+  base: 'flagship',
 });
