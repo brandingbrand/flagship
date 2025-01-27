385b1883d9400e96085038988eb79454998e54a6
Chris Herman <crherman7@gmail.com>
2025-01-27 16:38:13 -0500

    feat(react-navigation): minimum deps

--- apps/example/src/assets/index.ts
+++
@@ -1,8 +0,0 @@
-import {ImageRequireSource} from 'react-native';
-
-export default {
-  account: require('./tabAccount.png') as ImageRequireSource,
-  bag: require('./tabBag.png') as ImageRequireSource,
-  home: require('./tabHome.png') as ImageRequireSource,
-  shop: require('./tabShop.png') as ImageRequireSource,
-};
--- apps/example/src/routes/cart.discount.$discount.ts
+++
@@ -1,7 +0,0 @@
-import {Alert} from 'react-native';
-
-function applyDiscount(...args: any) {
-  Alert.alert('Apply Discount', JSON.stringify(args));
-}
-
-export default applyDiscount;
