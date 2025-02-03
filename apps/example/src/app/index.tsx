17f20c88d1b7071d2ae0d8a4e2cc0f209f72407f
Chris Herman <crherman7@gmail.com>
2025-02-03 15:44:41 -0500

    feat(stack): bottom stack navigator

--- apps/example/src/app/index.tsx
+++ apps/example/src/app/index.tsx
@@ -10,45 +10,39 @@ import {WishlistStackNavigator} from './routes/wishlist';
 
 const BottomTab = createBottomTabNavigator();
 
-function BottomTabNavigator() {
-  return (
-    <BottomTab.Navigator>
-      <BottomTab.Screen
-        name="DiscoverStack"
-        component={DiscoverStackNavigator}
-        options={{headerShown: false, title: 'Discover'}}
-      />
-      <BottomTab.Screen
-        name="ShopStack"
-        component={ShopStackNavigator}
-        options={{headerShown: false, title: 'Shop'}}
-      />
-      <BottomTab.Screen
-        name="WishlistStack"
-        component={WishlistStackNavigator}
-        options={{headerShown: false, title: 'Wishlist'}}
-      />
-      <BottomTab.Screen
-        name="CartStack"
-        component={CartStackNavigator}
-        options={{headerShown: false, title: 'Cart'}}
-      />
-      <BottomTab.Screen
-        name="AccountStack"
-        component={AccountStackNavigator}
-        options={{headerShown: false, title: 'Account'}}
-      />
-    </BottomTab.Navigator>
-  );
-}
-
 export function App() {
   return (
     <NavigationContainer
       onUnhandledAction={action =>
         console.error('Unhandled navigation action:', action)
       }>
-      <BottomTabNavigator />
+      <BottomTab.Navigator>
+        <BottomTab.Screen
+          name="DiscoverStack"
+          component={DiscoverStackNavigator}
+          options={{headerShown: false, title: 'Discover'}}
+        />
+        <BottomTab.Screen
+          name="ShopStack"
+          component={ShopStackNavigator}
+          options={{headerShown: false, title: 'Shop'}}
+        />
+        <BottomTab.Screen
+          name="WishlistStack"
+          component={WishlistStackNavigator}
+          options={{headerShown: false, title: 'Wishlist'}}
+        />
+        <BottomTab.Screen
+          name="CartStack"
+          component={CartStackNavigator}
+          options={{headerShown: false, title: 'Cart'}}
+        />
+        <BottomTab.Screen
+          name="AccountStack"
+          component={AccountStackNavigator}
+          options={{headerShown: false, title: 'Account'}}
+        />
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
