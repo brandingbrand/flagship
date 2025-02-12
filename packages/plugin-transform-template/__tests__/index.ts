41df3c73f80b12a43fd47592e925ce4360c29485
Chris Herman <crherman7@gmail.com>
2025-02-12 14:15:56 -0500

    feat(pkg): setup plugins

---
+++ packages/plugin-transform-template/__tests__/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment-options {"requireTemplate": true}
+ */
+
+/// <reference types="@brandingbrand/code-jest-config" />
+
+describe('plugin', () => {
+  it('ios', async () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('android', async () => {
+    expect(true).toBeTruthy();
+  });
+});
