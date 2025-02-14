be59edbd4064d7b672e640e927447b2dec9eb4a4
Chris Herman <crherman7@gmail.com>
2025-02-14 15:11:10 -0500

    feat(react-native): support up to 77

--- packages/plugin-transform-template/src/transforms/0.73/index.ts
+++ packages/plugin-transform-template/src/transforms/0.73/index.ts
@@ -1,12 +1,12 @@
 import {transforms072} from '../0.72';
 
-import {default as androidBuildGradle} from './android/build-gradle';
-import {default as androidMainApplication} from './android/main-application-kt';
+import {default as androidBuildGradleTransform} from './android/build-gradle';
+import {default as androidMainApplicationTransform} from './android/main-application-kt';
 import {default as iosPbxprojTransform} from './ios/project-pbxproj';
 
 export const transforms073 = {
   ...transforms072,
-  androidBuildGradle,
-  androidMainApplication,
+  androidBuildGradleTransform,
+  androidMainApplicationTransform,
   iosPbxprojTransform,
 };
--- packages/plugin-transform-template/src/transforms/index.ts
+++ packages/plugin-transform-template/src/transforms/index.ts
@@ -2,6 +2,8 @@ import {version} from '@brandingbrand/code-cli-kit';
 
 import {transforms072} from './0.72';
 import {transforms073} from './0.73';
+import {transforms075} from './0.75';
+import {transforms077} from './0.77';
 
 /**
  * Export version-specific transforms object that selects correct transforms based on version
@@ -9,4 +11,6 @@ import {transforms073} from './0.73';
 export const transforms = version.select({
   '0.72': transforms072,
   '0.73': transforms073,
+  '0.75': transforms075,
+  '0.77': transforms077,
 });
