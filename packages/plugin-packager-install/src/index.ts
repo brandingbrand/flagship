306db2bb541222b78613a8212a1fd0d0d6fc1e3e
Chris Herman <crherman7@gmail.com>
2025-02-18 16:36:12 -0500

    feat(pkg): update packager

--- packages/plugin-packager-install/src/index.ts
+++ packages/plugin-packager-install/src/index.ts
@@ -13,49 +13,53 @@ import {
  * Always installs gems and uses `bundle exec` for Ruby dependencies.
  */
 export default definePlugin({
-  /**
-   * The main function that executes the package installation process for iOS and Android platforms.
-   * @returns A Promise representing the completion of the package installation process.
-   */
-  common: async (_, options: PrebuildOptions): Promise<void> => {
-    // Dynamically import the execa library for running shell commands
+  ios: async (_, options: PrebuildOptions): Promise<void> => {
     const {execa} = await import('execa');
 
-    const runBundleInstall = async (platform: string, cwd: string) => {
-      try {
-        for await (const line of execa('bundle', ['exec', 'install'], {cwd})) {
-          logger.debug(line);
-        }
-        logger.log(`Installed Ruby gems for the ${platform} project`);
-      } catch (error: any) {
-        throw new Error(
-          `Error: failed to run "bundle exec install" for ${platform}: ${error.message}`,
-        );
+    try {
+      for await (const line of execa('bundle', ['exec', 'install'], {
+        cwd: path.project.resolve('ios'),
+      })) {
+        logger.debug(line);
       }
-    };
-
-    if (canRunAndroid(options)) {
-      await runBundleInstall('Android', path.project.resolve('android'));
+      logger.log('Installed Ruby gems for the iOS project');
+    } catch (error: any) {
+      throw new Error(
+        `Error: failed to run "bundle exec install" for iOS: ${error.message}`,
+      );
     }
 
-    if (canRunIOS(options)) {
-      await runBundleInstall('iOS', path.project.resolve('ios'));
+    try {
+      const podInstallOut = execa('bundle', ['exec', 'pod', 'install'], {
+        cwd: path.project.resolve('ios'),
+      });
 
-      try {
-        const podInstallOut = execa('bundle', ['exec', 'pod', 'install'], {
-          cwd: path.project.resolve('ios'),
-        });
-
-        for await (const line of podInstallOut) {
-          logger.debug(line);
-        }
-
-        logger.log('Installed pods for the iOS project');
-      } catch (error: any) {
-        throw new Error(
-          `Error: failed to run "bundle exec pod install" for iOS: ${error.message}`,
-        );
+      for await (const line of podInstallOut) {
+        logger.debug(line);
       }
+
+      logger.log('Installed pods for the iOS project');
+    } catch (error: any) {
+      throw new Error(
+        `Error: failed to run "bundle exec pod install" for iOS: ${error.message}`,
+      );
+    }
+  },
+
+  android: async (_, options: PrebuildOptions): Promise<void> => {
+    const {execa} = await import('execa');
+
+    try {
+      for await (const line of execa('bundle', ['exec', 'install'], {
+        cwd: path.project.resolve('android'),
+      })) {
+        logger.debug(line);
+      }
+      logger.log('Installed Ruby gems for the Android project');
+    } catch (error: any) {
+      throw new Error(
+        `Error: failed to run "bundle exec install" for Android: ${error.message}`,
+      );
     }
   },
 });
