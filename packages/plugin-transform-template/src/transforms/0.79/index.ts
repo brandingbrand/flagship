a88403ea18f85c273f6012ecd30e6af1345f6e32
georgepoladashvili <devs9gp@gmail.com>
2025-11-11 23:15:25 +0100

    feat(plugin-transform-template): ENG-6252 - added linkingDelegate method for RN 0.79 and above

---
+++ packages/plugin-transform-template/src/transforms/0.79/index.ts
@@ -0,0 +1,8 @@
+import {transforms077} from '../0.77';
+
+import {default as iosAppDelegateTransform} from './ios/app-delegate-swift';
+
+export const transforms079 = {
+  ...transforms077,
+  iosAppDelegateTransform,
+};
--- packages/plugin-transform-template/src/transforms/index.ts
+++ packages/plugin-transform-template/src/transforms/index.ts
@@ -4,6 +4,7 @@ import {transforms072} from './0.72';
 import {transforms073} from './0.73';
 import {transforms075} from './0.75';
 import {transforms077} from './0.77';
+import {transforms079} from './0.79';
 
 /**
  * Export version-specific transforms object that selects correct transforms based on version
@@ -13,4 +14,5 @@ export const transforms = version.select({
   '0.73': transforms073,
   '0.75': transforms075,
   '0.77': transforms077,
+  '0.79': transforms079,
 });
