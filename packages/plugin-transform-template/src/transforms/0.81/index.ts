a7826f582f77d620ccf0774554e31bac05095b21
Cameron Welter <cameron.welter@brandingbrand.com>
2025-12-05 13:18:41 -0500

    feat: support edgeToEdgeEnabled gradle property in RN81+

---
+++ packages/plugin-transform-template/src/transforms/0.81/index.ts
@@ -0,0 +1,8 @@
+import {transforms077} from '../0.77';
+
+import {default as androidGradlePropertiesTransform} from './android/gradle-properties';
+
+export const transforms081 = {
+  ...transforms077,
+  androidGradlePropertiesTransform,
+};
--- packages/plugin-transform-template/src/transforms/index.ts
+++ packages/plugin-transform-template/src/transforms/index.ts
@@ -5,6 +5,7 @@ import {transforms073} from './0.73';
 import {transforms075} from './0.75';
 import {transforms077} from './0.77';
 import {transforms079} from './0.79';
+import {transforms081} from './0.81';
 
 /**
  * Export version-specific transforms object that selects correct transforms based on version
@@ -15,4 +16,5 @@ export const transforms = version.select({
   '0.75': transforms075,
   '0.77': transforms077,
   '0.79': transforms079,
+  '0.81': transforms081,
 });
