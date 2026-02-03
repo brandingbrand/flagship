6974b5f374a4f8168251f7c8bf19f2f813c27942
Nick Burkhart <nick.burkhart@brandingbrand.com>
2026-02-03 09:26:48 -0500

    fix: new arch defaults for 0.82+ is ignored
    add warning to notify safe to remove

---
+++ packages/plugin-transform-template/src/transforms/0.82/index.ts
@@ -0,0 +1,10 @@
+import {transforms081} from '../0.81';
+
+import {default as androidGradlePropertiesTransform} from './android/gradle-properties';
+import {default as iosPodfileTransform} from './ios/podfile';
+
+export const transforms082 = {
+  ...transforms081,
+  androidGradlePropertiesTransform,
+  iosPodfileTransform,
+};
