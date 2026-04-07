74490ad4b2eb7e883d98ae2165e26ec0e05ef098
Cameron Welter <cameron.welter@brandingbrand.com>
2026-04-07 16:04:42 -0400

    feat(app-env): add prop to AsyncStorage to control parsing storage values

--- packages/app-env/src/app/screens/AsyncStorage.tsx
+++ packages/app-env/src/app/screens/AsyncStorage.tsx
@@ -1,40 +1,66 @@
 import storage from '@react-native-async-storage/async-storage';
+import {KeyValuePair} from '@react-native-async-storage/async-storage/lib/typescript/types';
 import React, {useCallback, useEffect, useMemo, useState} from 'react';
 
 import {DataView} from '../components/ui';
 import {defineDevMenuScreen} from '../lib/define-screen';
 
-export const AsyncStorageDevScreen = defineDevMenuScreen(
-  'AsyncStorage',
-  function AsyncStorageDevScreen() {
-    const [content, setContent] = useState('Loading...');
+interface AsyncStorageDevScreenProps {
+  /**
+   * If true, AsyncStorage values that are detected as
+   * JSON strings will be parsed and formatted for display.
+   *
+   * @default true
+   */
+  parseValues?: boolean;
+}
 
-    const fetchContent = useCallback(async () => {
-      const keys = await storage.getAllKeys();
-      const data = await storage.multiGet(keys);
-      setContent(JSON.stringify(data, null, 2));
-    }, []);
+function AsyncStorageDevScreenImpl({parseValues = true}: AsyncStorageDevScreenProps) {
+  const [content, setContent] = useState<string | readonly KeyValuePair[]>(
+    'Loading...',
+  );
 
-    const deleteAll = useCallback(async () => {
-      const keys = await storage.getAllKeys();
-      await storage.multiRemove(keys);
-      await fetchContent();
-    }, [fetchContent]);
+  const fetchContent = useCallback(async () => {
+    const keys = await storage.getAllKeys();
+    const data = await storage.multiGet(keys);
+    setContent(data);
+  }, []);
 
-    useEffect(() => {
-      fetchContent();
-    }, []);
+  const deleteAll = useCallback(async () => {
+    const keys = await storage.getAllKeys();
+    await storage.multiRemove(keys);
+    await fetchContent();
+  }, [fetchContent]);
 
-    const actions = useMemo(
-      () => [
-        {
-          label: 'Clear AsyncStorage',
-          onPress: deleteAll,
-        },
-      ],
-      [],
-    );
+  useEffect(() => {
+    fetchContent();
+  }, []);
 
-    return <DataView content={content} actions={actions} />;
-  },
-);
+  const actions = useMemo(
+    () => [
+      {
+        label: 'Clear AsyncStorage',
+        onPress: deleteAll,
+      },
+    ],
+    [],
+  );
+
+  return <DataView deepParse={parseValues} content={content} actions={actions} />;
+}
+
+export const AsyncStorageDevScreen =
+  defineDevMenuScreen<AsyncStorageDevScreenProps>(
+    'AsyncStorage',
+    AsyncStorageDevScreenImpl,
+  );
+
+export const createAsyncStorageDevScreen = (
+  props: AsyncStorageDevScreenProps,
+) =>
+  defineDevMenuScreen(
+    `AsyncStorage`,
+    function ConfiguredAsyncStorageDevScreen() {
+      return <AsyncStorageDevScreenImpl {...props} />;
+    },
+  );
